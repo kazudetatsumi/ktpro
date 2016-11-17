21625b3eba8fe2c27866a7db44a351caf6d12a0d
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2016-11-17 19:00:45 +0900

    cut-image.py modified

--- cut-image.py
+++ cut-image.py
@@ -4,8 +4,16 @@ import sys
 
 argvs = sys.argv
 im = Image.open(argvs[1])
-values=argvs[1].split(" ")
+values=argvs[1].split(".")
+values2=values[0].split("test")
 w,h = im.size
-im.crop((3,360,w-3,h-310)).save(values[2])
-#im.crop((3,80,w-3,h-33)).save(values[2])
+if int(values2[1])%2==0: 
+	box = (70,150, w-60, h-110)
+	box = (80,150, w-50, h-110)
+else:
+	box = (30,150, w-100, h-110)
+
+print w,h,values[0]
+#im.crop((50,50,w-100,h-100)).save(values[2])
+im.crop(box).save(values[0]+"rs.jpg")
 #im.crop((3,125,w-3,h-75)).save(values[2])
