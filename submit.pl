ce766c745e2131409f08ba9adb6a8325261dc45a
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2017-01-13 16:16:57 +0900

    modified submit.pl

--- submit.pl
+++ submit.pl
@@ -24,8 +24,10 @@ while(!eof(LS)){
   $totalsecond[$i]=$s[$i]+60*$m[$i]+60*60*$t[$i]+60*60*24*($dd[$i]+$mm[$i]*31+$yy[$i]*365);
   print "$dateset[0] $dateset[1] $dateset[2] $dateset[3]\n";
   print "$timeset[0] $timeset[1] $timeset[2] $timeset[3] $totalsecond[$i]\n";
-  $queue[$i]=$values[-2];$master[$i]=$values[-1];
-  chop($queue[$i]);chop($queue[$i]);
+  @values2=split(/\@/,$values[-2]);
+  $queue[$i]=$values2[-1];$master[$i]=$values[-1];
+  $ncore[$i]=$values[$#arr];
+  print "ncore, $ncore[$i]\n";
   }
 }
 $j=0;
@@ -36,41 +38,10 @@ for ($i=2;$i<=$numqueue;$i++){
 }
 for($i=1;$i<=$numqueue;$i++){
   if($totalsecond[$i] == $totalsecond[$lid]){
-    $j++;
-    $wid[$j]=$i;
+    $wid=$i;
   }
 }
-$numw=$j;
-#Grouping for dual core!!
-for($i=1;$i<=$numw;$i++){
-$test[$i]=0;
-$deg[$i]=1;
-}
-$ne=0;
-for($i=1;$i<=$numw;$i++){
-    
-    print "$test[$i]\n";
-    if ($test[$i] == 0){
-    $ne++;
-    $qname[$ne]=$queue[$wid[$i]];
-    for($j=$i+1;$j<=$numw;$j++){
-       if($queue[$wid[$j]] eq $queue[$wid[$i]]){
-          $test[$j] = 1;
-	  $deg[$ne]++;}
-    }
-  }
-}
-#######
-
-$nne=$ne;  
-$lapw0="lapw0:";
-for ($ne=1;$ne<=$nne;$ne++){
-  $lapw0="$lapw0"."$qname[$ne]:$deg[$ne] ";
-}
-print "$lapw0 \n";
 open(MAC,">$machinefile");
-#  print MAC "$lapw0\n";
-for ($i=1;$i<=$numw;$i++){
-  print MAC "1:$queue[$wid[$i]] \n";
-#  print MAC "$queue[$wid[$i]] \n";
+for ($i=1;$i<=$ncore[$wid];$i++){
+  print MAC "1:$queue[$wid] \n";
 }     
