b3e715fd24f2932b54103ca421f9669893cbdce7
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2017-11-17 16:50:54 +0900

    analyse_gamma_gamma

--- analyse_gamma_gamma.py
+++ analyse_gamma_gamma.py
@@ -4,17 +4,11 @@ import matplotlib.pyplot as plt
 import h5py
 import math
 homedir = "/home/kazu/"
-cdir = homedir + "/asi3n4/phono3py_112_fc2_334_sym_monk_shift/"
-sdir = homedir + "/bsi3n4_m/phono3py_113_fc2_338_sym_monk_shift/"
 gdir = homedir + "/gamma-si3n4-unit/phono3py_111_fc2_222_sym_monk_k-shift/"
 Temp = 300
-max_freq = 5
+max_freq = 30
 fs = 9
-c = cdir + "noiso/kappa-m141416.noiso.hdf5"
-s = sdir + "noiso/kappa-m141432.noiso.hdf5"
 g = gdir + "noiso/kappa-m121212.hdf5"
-cv = cdir + "qpoints.hdf5"
-sv = sdir + "qpoints.hdf5"
 gv = gdir + "qpoints.hdf5"
 
 
@@ -77,48 +71,44 @@ def project_eigenvec(edata):
     for i in range(0,datasize[0]):
         for j in range(0,datasize[2]):
             amp = 0
-            for k in range(0,datasize[1]):
-                if k % 3 == 0 or k % 3 == 1:
-                    amp += abs(edata[i,k,j])**2
-            ampdata[i,j]=amp
+            tmpamp = []
+            tmpamp.append(get_amp(edata[i,:,j],1,1,1))
+            tmpamp.append(get_amp(edata[i,:,j],-1,1,1))
+            tmpamp.append(get_amp(edata[i,:,j],1,-1,1))
+            tmpamp.append(get_amp(edata[i,:,j],1,1,-1))
+            ampdata[i,j]=max(tmpamp)
+            print max(tmpamp)
     return(ampdata)
 
-def trans(qpoint,edata):
-    datasize=edata.shape
-    longidata = np.zeros((datasize[0],datasize[2]))
-    for i in range(0,datasize[0]):
-        norm = (qpoint[i,0]**2 + qpoint[i,1]**2 + qpoint[i,2]**2)**0.5
-        if norm > 0:
-           for j in range(0,datasize[2]):
-               longi = 0
-               for k in range(0,datasize[1]):
-                   if k % 3 == 0:
-                      longi += (abs(edata[i,k,j]*qpoint[i,0] + edata[i,k+1,j]*qpoint[i,1] + edata[i,k+2,j]*qpoint[i,2])/norm)**2
-               longidata[i,j]=longi
-    return(longidata)
+def get_amp(data,l,m,n):
+    datasize = data.shape
+    amp = 0
+    for k in range(0,datasize[0]):
+        if k % 3 == 0:
+           amp += (abs(data[k]*l + data[k+1]*m + data[k+2]*n))**2 / (l**2 + m**2 + n**2)
+    return(1 - amp)
 
     
 
-def select_mode(omega,gamma,sqamp,lamp):
+def select_mode(omega,gamma,sqamp):
     freqs = []
     gammas = []
     sqamps = []
-    lamps = []
-    for f, g, s, l in zip(omega,gamma,sqamp,lamp):
+    for f, g, s in zip(omega,gamma,sqamp):
         condition = f < max_freq 
         _f = np.extract(condition,f)
         _g = np.extract(condition,g)
         _s = np.extract(condition,s)
-        _l = np.extract(condition,l)
         freqs += list(_f)
         gammas += list(_g)
         sqamps += list(_s)
-        lamps += list(_l)
     f1 = np.array(freqs).ravel()
     g1 = np.array(gammas).ravel()
     s1 = np.array(sqamps).ravel()
-    l1 = np.array(lamps).ravel()
-    return(f1,g1,s1,l1)
+    f1 = np.append(f1,0)
+    g1 = np.append(g1,0)
+    s1 = np.append(s1,0)
+    return(f1,g1,s1)
 
 def caserun(casefile,vcasefile,n,phase):
     omegak,gammak,qpointk=parse_gamma(casefile,Temp)
@@ -126,45 +116,21 @@ def caserun(casefile,vcasefile,n,phase):
     check(omegak,omegaq)
     check(qpointk,qpointq)
     sqamp=project_eigenvec(eigenvecq) 
-    longiamp=trans(qpointq,eigenvecq)
-    omega1d,gamma1d,sqamp1d,longiamp1d=select_mode(omegak,gammak,sqamp,longiamp)
-    plt.subplot(2,2,n)
-    plt.scatter(omega1d,gamma1d,c=longiamp1d,linewidth=0.01,s=5, label=phase)
+    omega1d,gamma1d,sqamp1d=select_mode(omegak,gammak,sqamp)
+    plt.scatter(omega1d,gamma1d,c=sqamp1d,linewidth=0.01,s=15, label=phase)
     plt.yscale("log")
-    plt.ylim(0.0005,0.0050)
+    plt.ylim(0.0005,0.350)
     plt.xlabel('omega / THz')
     plt.ylabel('gamma')
     plt.legend()
-    plt.colorbar(label='sum of squares of eigenvector component along q')
-    plt.subplot(2,2,n+2)
-    plt.scatter(omega1d,gamma1d,c=sqamp1d,linewidth=0.01,s=5, label=phase)
-    if phase == "beta":
-        x = 0.1*np.arange(0,50)
-        y = 0.001*np.exp(0.16*(x-0.5)) 
-        plt.plot(x,y)
-        n_up = 0
-        n_dn = 1
-        for omega,gamma in zip(omega1d,gamma1d):
-            if gamma >=  0.001*math.exp(0.16*(omega-0.5)):
-                    n_up += 1
-            else:
-                    n_dn += 1
-        print "num of up states:",n_up
-        print "num of dn states:",n_dn
-
-    plt.yscale("log")
-    plt.ylim(0.0005,0.0050)
-    plt.xlabel('omega / THz')
-    plt.ylabel('gamma')
-    plt.legend()
-    plt.colorbar(label='sum of squares of eigenvector x_y_component')
+    plt.colorbar(label='sum of squares of eigenvector comp on {111}')
 
 
 def run():
     plt.figure(figsize=(12,12))
-    caserun(c,cv,1,"alpha")
-    caserun(s,sv,2,"beta")
-    #caserun(g,gv,3,"gamma")
+    #caserun(c,cv,1,"alpha")
+    #caserun(s,sv,2,"beta")
+    caserun(g,gv,1,"gamma")
 
 run()
 plt.show()
