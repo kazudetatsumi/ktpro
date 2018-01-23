a8767956caf1182e9eb9b552656b8399c51204e2
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2018-01-23 15:27:22 +0900

    mals

--- mals_dm4.py
+++ mals_dm4.py
@@ -6,14 +6,20 @@ import matplotlib.cm as cm
 import math
 from scipy.interpolate import griddata
 import matplotlib
-matplotlib.font_manager._rebuild()
+import sys
 
+#matplotlib.font_manager._rebuild()
+args = sys.argv
 est=int(round((375-175)*2.5))
 efi=int(round((650-225)*2.5))
+#est=int(round((375-175)))
+#efi=int(round((650-225)))
 numcom=2
-maxcycle=1000
-datanum=17
-IsAligned='true'
+maxcycle=10
+datanum=21
+#datanum=args[1]
+IsAligned='false'
+#IsAligned=args[2]
 
 if IsAligned == 'true':
     f="/home/kazu/LaNi5_dm4/SI data ("+str(datanum)+")Signal SI_spike_bkg_rmvd (aligned).dm4"
@@ -21,7 +27,9 @@ if IsAligned == 'true':
 else:
     f="/home/kazu/LaNi5_dm4/SI data ("+str(datanum)+")Signal SI_spike_bkg_rmvd.dm4"
     outname="result_no"+str(datanum)+"_comp"+str(numcom)+"_cycle"+str(maxcycle)
-g="/home/kazu/LaNi5_dm4/ADF Image (SI Survey)"+str(datanum)+".tif"
+#g="/home/kazu/LaNi5_dm4/ADF Image (SI Survey)"+str(datanum)+".tif"
+g="/home/kazu/LaNi5_dm4/ADF Image"+str(datanum)+" (SI Survey).tif"
+h="/home/kazu/LaNi5_dm4/ADF Image"+str(datanum)+".dm4"
 
 
 
@@ -37,6 +45,19 @@ def get_3ddata(filename):
 
     np_array = np.array(dm4data.read_tag_data(image_tag))
     np_array = np.reshape(np_array, (ZDim, YDim, XDim))
+    return(np_array)
+
+def get_2ddata(filename):
+    dm4data = dm4reader.DM4File.open(filename)
+    tags = dm4data.read_directory()
+    image_data_tag = tags.named_subdirs['ImageList'].unnamed_subdirs[1].named_subdirs['ImageData']
+    image_tag = image_data_tag.named_tags['Data']
+
+    XDim = dm4data.read_tag_data(image_data_tag.named_subdirs['Dimensions'].unnamed_tags[0])
+    YDim = dm4data.read_tag_data(image_data_tag.named_subdirs['Dimensions'].unnamed_tags[1])
+
+    np_array = np.array(dm4data.read_tag_data(image_tag))
+    np_array = np.reshape(np_array, (YDim, XDim))
     return(np_array)
 
 
@@ -63,21 +84,22 @@ def mals(data,n,mc):
         sr = np.linalg.norm(r)
         #print sr
         srs.append(sr)
-        #if nn > 1:
-        #    print srs[nn-1] - sr
+        if nn > maxcycle*0.2:
+            print srs[nn-1] - sr
         nn = nn+1
     print sr
     b2d = np.reshape(b,[n,dim[1],dim[2]])
     print b2d.shape
     return(a,b2d,srs)
 
-def plotter(s,n):
+def plotter(s):
+    n=numcom
     #subplot2grid(shape, loc, rowspan=1, colspan=1, fig=None, **kwargs
     #ax1 = plt.subplot2grid((6,1), (0,0), rowspan=1, colspan=1)
     #ax2 = plt.subplot2grid((6,1), (1,0), rowspan=4, colspan=1)
     #ax3 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
     #plt.subplot(1,n+2,1)
-    plt.subplot2grid((1,n+3),(0,0),rowspan=1, colspan=2)
+    plt.subplot2grid((4,n+3),(0,0),rowspan=1, colspan=2)
     for i in range(0,n):
        labelname="comp. #"+str(i)
        plt.plot(s[:,i],label=labelname)
@@ -86,27 +108,37 @@ def plotter(s,n):
     plt.title("spectra")
     plt.legend()
 
-def plotter2d(c,n):
+def plotter2d(c,a):
+    n=numcom
     for i in range(0,n):
     #   plt.subplot(1,n+2,i+2)
     #plt.subplot2grid((1,4),(0,2),rowspan=1, colspan=1)
-       plt.subplot2grid((1,n+3),(0,i+2),rowspan=1, colspan=1)
+       plt.subplot2grid((4,n+3),(0,i+2),rowspan=1, colspan=1)
        labelname="comp. #"+str(i)
        plt.imshow(c[i,:,:], cmap = cm.gray, interpolation = 'none',label="comp. #"+str(i))
        plt.title("map of comp. #"+str(i))
        plt.tick_params(labelbottom='off',bottom='false',labelleft='off',left='false')
+    plt.subplot2grid((4,n+3),(0,n+2),rowspan=1, colspan=1)
+    plt.imshow(a, cmap = cm.gray, interpolation = 'none',label="comp. #"+str(i))
+    plt.tick_params(labelbottom='off',bottom='false',labelleft='off',left='false')
+    plt.title("ADF image")
     #plt.subplot2grid((1,4),(0,3),rowspan=1, colspan=1)
     #plt.subplot(1,3,3)
     #plt.imshow(c[1,:,:], cmap = cm.gray, interpolation = 'none',label="comp. #2")
     #plt.title("map of comp. #2")
     
-def plotteradf(tifname,n):
+def plotteradfsurvey(tifname,n):
+    n=numcom
     adfimage=plt.imread(tifname)
-    plt.subplot2grid((1,n+3),(0,n+2),rowspan=1, colspan=1)
+    #plt.subplot2grid((4,n+3),(0,n+2),rowspan=1, colspan=1)
+    #plt.tick_params(labelbottom='off',bottom='false',labelleft='off',left='false')
+    #plt.title("ADF survey image")
+    #plt.imshow(adfimage)
+    plt.subplot2grid((4,n+3),(1,2),rowspan=3, colspan=3)
     #plt.subplot(1,n+2,n+2)
     plt.imshow(adfimage)
     #plt.subplots_adjust(bottom=0.1,top=0.82,wspace=0.1)
-    plt.subplots_adjust(top=0.8,wspace=0.1)
+    plt.subplots_adjust(top=0.95,hspace=0.01,wspace=0.01)
     plt.tick_params(labelbottom='off',bottom='false',labelleft='off',left='false')
     plt.title("ADF survey image")
     
@@ -116,12 +148,13 @@ def run():
     ximage = get_3ddata(f)
     ximage2 = ximage[est:efi,:,:]
     adat,bdat,sumress = mals(ximage2,numcom,maxcycle)
-    plt.figure(figsize=(2.5*(numcom+3),2.5))
+    adfimage = get_2ddata(h)
+    plt.figure(figsize=(2.5*(numcom+3),2.5*4))
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.suptitle(outname)
-    plotter(adat,numcom)
-    plotter2d(bdat,numcom)
-    plotteradf(g,numcom)
+    plotter(adat)
+    plotter2d(bdat,adfimage)
+    plotteradfsurvey(g,numcom)
     #plt.show()
     plt.savefig(outname+".eps")
 
