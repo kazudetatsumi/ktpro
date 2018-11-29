2c3b9b36cfd6c41cd4e2d6c654080d7631940529
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2018-11-29 17:58:55 +0900

    analyze_gamma_per_mode

--- analyse_gamma_nolog_per_mode.py
+++ analyse_gamma_nolog_per_mode.py
@@ -20,7 +20,7 @@ cv = cdir + "qpoints.hdf5"
 sv = sdir + "qpoints.hdf5"
 gv = gdir + "qpoints.hdf5"
 
-#plt.style.use('classic')
+plt.style.use('classic')
 mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
 plt.rcParams['font.family'] = 'Times New Roman'
@@ -28,6 +28,8 @@ plt.rcParams['font.size'] = 14
 plt.rcParams['axes.linewidth'] = 1
 plt.rcParams['ytick.major.width'] = 1
 plt.rcParams['ytick.major.size'] = 5
+fig, ax = plt.subplots(2, 2, figsize=(12, 12))
+
 
 def parse_gamma(filename, temp):
     f = h5py.File(filename, 'r')
@@ -40,38 +42,14 @@ def parse_gamma(filename, temp):
     gamma_k = f["gamma"][tindex, ]
     omega_k = f["frequency"][:, :]
     qpoint_k = f["qpoint"][:]
-    # print np.array(omega_k).shape
-    # print np.array(gamma_k).shape
-    # print np.array(qpoint_k).shape
-    # print omega_k[0,0:32]
-
     return(omega_k, gamma_k, qpoint_k)
 
 
 def parse_eigenvec(filename):
-    # freqs = []
-    # mode_prop = []
     f = h5py.File(filename, 'r')
     omega_q = f["frequency"][:, :]
     qpoint_q = f["qpoint"][:]
     eigenvec_q = f["eigenvector"][:]
-
-    # for freq, e in zip(omega, eigenvec):
-    #    condition = freq < max_freq
-    #    _freq = np.extract(condition, freq)
-    #    _e = np.extract(condition, e)
-    #    freqs += list(_freq)
-    #    mode_prop += list(_e)
-    # gamma1=np.array(mode_prop).ravel()
-    # omega1=np.array(freqs).ravel()
-    # return(omega1,gamma1,qpoint)
-    # omega=np.array(omega)
-    # mode_prop=np.array(eigenvec)
-    # print np.array(omega_q).shape
-    # print np.array(eigenvec_q).shape
-    # print np.array(qpoint_q).shape
-    # print omega_q[0,0:32]
-
     return(omega_q, eigenvec_q, qpoint_q)
 
 
@@ -137,6 +115,51 @@ def select_mode(omega, gamma, sqamp, lamp):
     return(f1, g1, s1, l1)
 
 
+
+def band_by_band(omega, gamma, sqamp, lamp, phase, n, markstyle):
+    freqs = []
+    gammas = []
+    sqamps = []
+    lamps = []
+    for f, g, s, l in zip(omega, gamma, sqamp, lamp):
+        condition = f < max_freq
+        _f = np.extract(condition, f)
+        _g = np.extract(condition, g)
+        _s = np.extract(condition, s)
+        _l = np.extract(condition, l)
+        freqs += list(_f)
+        gammas += list(_g)
+        sqamps += list(_s)
+        lamps += list(_l)
+    f1 = np.array(freqs).ravel()
+    g1 = np.array(gammas).ravel()
+    s1 = np.array(sqamps).ravel()
+    l1 = np.array(lamps).ravel()
+    ax[n, 0].scatter(f1, 1.0 / (4 * np.pi * g1), c=l1, linewidth=0.01, s=18,
+                     label=phase, cmap='magma', marker=markstyle)
+    ax[n, 0].set_ylim(0, 130)
+    ax[n, 0].set_yticks([0, 20, 40, 60, 80, 100, 120, 130])
+    ax[n, 0].set_xlim(0, 5)
+    ax[n, 0].set_xticks([0, 1, 2, 3, 4, 5])
+    ax[n, 1].scatter(f1, 1.0 / (4 * np.pi * g1), c=s1, linewidth=0.01, s=18,
+                     label=phase, cmap='magma', marker=markstyle)
+    ax[n, 1].set_ylim(0, 130)
+    ax[n, 1].set_yticks([0, 20, 40, 60, 80, 100, 120, 130])
+    ax[n, 1].set_xlim(0, 5)
+    ax[n, 1].set_xticks([0, 1, 2, 3, 4, 5])
+    if phase == "beta":
+        n_up = 0
+        n_dn = 0
+        for f, g in zip(f1, g1):
+            if g > 0:
+                if 1.0 / (4 * np.pi * g) >= (-30*f + 390) / (2 * np.pi):
+                    n_up += 1
+                else:
+                    n_dn += 1
+        print "num of up states:", n_up
+        print "num of dn states:", n_dn
+
+
 def caserun(casefile, vcasefile, n, phase):
     omegak, gammak, qpointk = parse_gamma(casefile, Temp)
     omegaq, eigenvecq, qpointq = parse_eigenvec(vcasefile)
@@ -144,53 +167,63 @@ def caserun(casefile, vcasefile, n, phase):
     check(qpointk, qpointq)
     sqamp = project_eigenvec(eigenvecq)
     longiamp = trans(qpointq, eigenvecq)
-    omega1d, gamma1d, sqamp1d, longiamp1d = select_mode(omegak, gammak, sqamp,
-                                                        longiamp)
-    plt.subplot(2, 2, n)
-    plt.scatter(omega1d, 1.0 / (4 * np.pi * gamma1d), c=longiamp1d,
-                linewidth=0.01, s=5,
-                label=phase, cmap='magma')
-    plt.tick_params(which='both', tickdir='in')
-    plt.ylim(0, 110)
-    plt.yticks([0, 20, 40, 60, 80, 100])
-    plt.xlim(-0.2, 5.2)
-    plt.xlabel('omega / THz')
-    plt.ylabel('tau')
-    plt.colorbar(label='sum of squares of eigenvector component along q')
-    plt.subplot(2, 2, n+2)
-    plt.scatter(omega1d, 1.0 / (4 * np.pi * gamma1d), c=sqamp1d,
-                linewidth=0.01, s=5, label=phase, cmap='magma')
-    plt.clim(0.0, 1.2)
-    plt.tick_params(which='both', tickdir='in')
+    band_by_band(omegak[:, 0], gammak[:, 0], sqamp[:, 0], longiamp[:, 0],
+                 phase, n, "o")
+    band_by_band(omegak[:, 1], gammak[:, 1], sqamp[:, 1], longiamp[:, 2],
+                 phase, n, "v")
+    band_by_band(omegak[:, 2], gammak[:, 2], sqamp[:, 2], longiamp[:, 2],
+                 phase, n, "d")
     if phase == "beta":
         x = 0.1*np.arange(0, 51)
         y = (-30*x + 390) / (2 * np.pi)
-        plt.plot(x, y, color='k', linewidth=1)
-        n_up = 1
-        n_dn = 0
-        for omega, gamma in zip(omega1d, gamma1d):
-            if gamma > 0:
-                if 1 / (4 * np.pi / gamma) >= (-30*omega + 390) / (2 * np.pi):
-                    n_up += 1
-                else:
-                    n_dn += 1
-        print "num of up states:", n_up
-        print "num of dn states:", n_dn
+        ax[n, 1].plot(x, y, color='k', linewidth=1)
+    #omega1d, gamma1d, sqamp1d, longiamp1d = select_mode(omegak, gammak, sqamp,
+    #                                                    longiamp)
+    #plt.subplot(2, 2, n)
+    #plt.scatter(omega1d, 1.0 / (4 * np.pi * gamma1d), c=longiamp1d,
+    #            linewidth=0.01, s=5,
+    #            label=phase, cmap='magma')
+    #plt.tick_params(which='both', tickdir='in')
+    #plt.ylim(0, 110)
+    #plt.yticks([0, 20, 40, 60, 80, 100])
+    #plt.xlim(-0.2, 5.2)
+    #plt.xlabel('omega / THz')
+    #plt.ylabel('tau')
+    #plt.colorbar(label='sum of squares of eigenvector component along q')
+    #plt.subplot(2, 2, n+2)
+    #plt.scatter(omega1d, 1.0 / (4 * np.pi * gamma1d), c=sqamp1d,
+    #            linewidth=0.01, s=5, label=phase, cmap='magma')
+    #plt.clim(0.0, 1.2)
+    #plt.tick_params(which='both', tickdir='in')
+    #if phase == "beta":
+    #    x = 0.1*np.arange(0, 51)
+    #    y = (-30*x + 390) / (2 * np.pi)
+    #    plt.plot(x, y, color='k', linewidth=1)
+    #    n_up = 1
+    #    n_dn = 0
+    #    for omega, gamma in zip(omega1d, gamma1d):
+    #        if gamma > 0:
+    #            if 1 / (4 * np.pi / gamma) >= (-30*omega + 390) / (2 * np.pi):
+    #                n_up += 1
+    #            else:
+    #                n_dn += 1
+     #   print "num of up states:", n_up
+     #   print "num of dn states:", n_dn
 
     # plt.yscale("log")
-    plt.ylim(0, 130)
-    plt.yticks([0, 20, 40, 60, 80, 100, 120])
-    plt.xlim(0, 5.0)
-    plt.xticks([0, 1, 2, 3, 4, 5])
-    plt.xlabel('omega / THz')
-    plt.ylabel('tau')
-    plt.colorbar(label='sum of squares of eigenvector x_y_component')
+    #plt.ylim(0, 130)
+    #plt.yticks([0, 20, 40, 60, 80, 100, 120])
+    #plt.xlim(0, 5.0)
+    #plt.xticks([0, 1, 2, 3, 4, 5])
+    #plt.xlabel('omega / THz')
+    #plt.ylabel('tau')
+    #plt.colorbar(label='sum of squares of eigenvector x_y_component')
 
 
 def run():
-    plt.figure(figsize=(12, 9.73))
-    caserun(c, cv, 1, "alpha")
-    caserun(s, sv, 2, "beta")
+    #plt.figure(figsize=(12, 9.73))
+    caserun(c, cv, 0, "alpha")
+    caserun(s, sv, 1, "beta")
     # caserun(g,gv,3,"gamma")
     plt.savefig("tst_plot.pdf")
 
--- bandplot.py
+++ bandplot.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 
 plt.style.use('classic')
 plt.rcParams['font.family'] = 'Times New Roman'
-plt.rcParams['font.size'] = 18
-plt.rcParams['axes.linewidth'] = 2
-plt.rcParams['ytick.major.width'] = 2
+plt.rcParams['font.size'] = 12
+plt.rcParams['axes.linewidth'] = 1
+plt.rcParams['ytick.major.width'] = 1
 plt.rcParams['ytick.major.size'] = 5
+fig, ax = plt.subplots(3, 2, figsize=(6, 10))
 
 
 def parse_band(bfile):
@@ -19,40 +20,36 @@ def parse_band(bfile):
     return(xdata, ydata)
 
 
-def plotdata(x, y, i):
+def plotdata(x, y, i, j):
     ydim = y.shape
-    plt.subplot(4, 2, i, aspect=0.014)
-    plt.axvline(x[100], color='k')
-    for j in range(0, ydim[1]):
-        plt.ylim(0, 34)
-        plt.xlim(min(x), max(x))
-        plt.xticks([min(x), max(x)])
-        plt.plot(x, y[:, j], color='k', linewidth=1.25)
+    for k in range(0, ydim[1]):
+        if j == 1:
+            ax[i, j].axvline(x[100], color='k', linewidth=0.025)
+        ax[i, j].set_ylim(0, 34)
+        ax[i, j].set_xlim(min(x), max(x))
+        ax[i, j].set_xticks([min(x), max(x)])
+        ax[i, j].set_aspect(0.009)
+        ax[i, j].plot(x, y[:, k], color='k', linewidth=1)
 
 
 def caserun(bfile, M):
     xdata, ydata = parse_band(bfile)
     x = xdata[0, :]
     y = ydata[0, :, :]
-    plotdata(x, y, 1+M*2)
+    plotdata(x, y, M, 0)
     x = np.concatenate((xdata[2, :], xdata[3, :]), axis=0)
     y = np.concatenate((ydata[2, :, :], ydata[3, :, :]), axis=0)
-    plotdata(x, y, 2+M*2)
+    plotdata(x, y, M, 1)
 
 
 def run():
-    plt.figure(figsize=(6, 18))
     cbfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/band.hdf5"
     caserun(cbfile, 0)
     sbfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/band.hdf5"
     caserun(sbfile, 1)
     gbfile = "/home/kazu/gamma-si3n4-unit/phono3py_111_fc2_222_sym_monk_k-shift/band.hdf5"
     caserun(gbfile, 2)
-
+    plt.savefig("band-alpha-beta-gamma2.eps")
 
 run()
 plt.show()
-
-#plt.savefig("band-alpha-beta-gamma.eps")
-
-
