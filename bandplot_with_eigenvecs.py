4054e64e71940f7b232607f4d674c8c9e4947fc8
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-06-13 11:19:34 +0900

    bandplot_eigenvecs.py

--- bandplot_with_eigenvecs.py
+++ bandplot_with_eigenvecs.py
@@ -33,7 +33,20 @@ def plotdata(x, y, z, i, j, title):
     ax[i, j].set_title(title)
     sc = ax[i, j].scatter(x, y, c=z, vmin=0, vmax=1, linewidth=0.01, s=1)
     #plt.colorbar(sc)
-    ax[i,j].set_facecolor('k')
+    ax[i, j].set_facecolor('k')
+
+
+def get_z(atomlist, direction, zdata):
+    z = np.zeros_like(zdata[0, :, 0, :])
+    if direction == "z":
+        for an in atomlist:
+            print an * 3 - 1
+            z += (abs(zdata[0, :, an * 3 - 1, :]))**2
+    elif direction == "xy":
+        for an in atomlist:
+            z += (abs(zdata[0, :, an * 3 - 2, :]))**2
+            z += (abs(zdata[0, :, an * 3 - 3, :]))**2
+    return z
 
 
 def caserun(bfile, M, K, title):
@@ -41,11 +54,21 @@ def caserun(bfile, M, K, title):
     x = xdata[0, :]
     y = ydata[0, :, :]
     if title == "beta_Nz":
-        z = (abs(zdata[0, :, 41, :]))**2 + (abs(zdata[0, :, 29, :]))**2  
+        atomlist = [10, 14]
+        z = get_z(atomlist, 'z', zdata)
+        #z = (abs(zdata[0, :, 41, :]))**2 + (abs(zdata[0, :, 29, :]))**2  
+    elif title == "beta_Nxy":
+        atomlist = [10, 14]
+        z = get_z(atomlist, 'xy', zdata)
+        #z = (abs(zdata[0, :, 39, :]))**2 + (abs(zdata[0, :, 40, :]))**2 + (abs(zdata[0, :, 27, :]))**2 + (abs(zdata[0, :, 28, :]))**2 
     elif title == "alpha_Nz":
-        z = (abs(zdata[0, :, 38, :]))**2 + (abs(zdata[0, :, 62, :]))**2 + (abs(zdata[0, :, 59, :]))**2 + (abs(zdata[0, :, 83, :]))**2 
-    elif title == "alpha_Nz_near_void":
-        z = (abs(zdata[0, :, 59, :]))**2 + (abs(zdata[0, :, 83, :]))**2  
+        atomlist = [13, 20, 21, 28]
+        z = get_z(atomlist, 'z', zdata)
+        #z = (abs(zdata[0, :, 38, :]))**2 + (abs(zdata[0, :, 62, :]))**2 + (abs(zdata[0, :, 59, :]))**2 + (abs(zdata[0, :, 83, :]))**2 
+    elif title == "alpha_Nxy":
+        atomlist = [13, 20, 21, 28]
+        z = get_z(atomlist, 'xy', zdata)
+        #z = (abs(zdata[0, :, 59, :]))**2 + (abs(zdata[0, :, 83, :]))**2  
     #print zdata[0, 0, 41, 0]
     #print (abs(zdata[0, 10, 10, 1]))**2    
     #print z[10,10]
@@ -69,11 +92,11 @@ def run():
     cbfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/band.hdf5"
     caserun(cbfile, 0, 0, "alpha_Nz")
     cbfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/band.hdf5"
-    caserun(cbfile, 0, 1, "alpha_Nz_near_void")
+    caserun(cbfile, 0, 1, "alpha_Nxy")
     sbfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/band.hdf5"
     caserun(sbfile, 1, 0, "beta_Nz")
     sbfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/band.hdf5"
-    caserun(sbfile, 1, 1, "beta_Nz")
+    caserun(sbfile, 1, 1, "beta_Nxy")
     #plt.savefig("band-alpha-beta-gamma2.eps")
 
 run()
