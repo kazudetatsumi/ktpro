4bac25e538184780307ce3ba1545ee5d01f15fe7
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-06-17 19:36:11 +0900

    bandplot_eigenvecs

--- bandplot_with_eigenvecs.py
+++ bandplot_with_eigenvecs.py
@@ -4,6 +4,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
+import matplotlib.cm as cm
 
 plt.style.use('classic')
 plt.rcParams['font.family'] = 'Times New Roman'
@@ -11,7 +12,7 @@ plt.rcParams['font.size'] = 12
 plt.rcParams['axes.linewidth'] = 1
 plt.rcParams['ytick.major.width'] = 1
 plt.rcParams['ytick.major.size'] = 5
-fig, ax = plt.subplots(2, 4, figsize=(16, 14))
+fig, ax = plt.subplots(2, 2, figsize=(14, 14))
 
 
 def parse_band(bfile):
@@ -22,7 +23,8 @@ def parse_band(bfile):
     return(xdata, ydata, zdata)
 
 
-def plotdata(x, y, z, i, j, title):
+
+def scatterplotdata(x, y, z, i, j, title):
     ydim = y.shape
     ax[i, j].axvline(x[100], color='k', linewidth=0.025)
     ax[i, j].set_ylim(0, 34)
@@ -30,8 +32,37 @@ def plotdata(x, y, z, i, j, title):
     ax[i, j].set_xticks([min(x), max(x)])
     ax[i, j].set_aspect(0.009)
     ax[i, j].set_title(title)
-    sc = ax[i, j].scatter(x, y, c=z, vmin=0, vmax=1, linewidth=0.01, s=1, cmap='binary_r')
+    sc = ax[i, j].scatter(x, y, c=z, vmin=0, vmax=1, linewidth=0.01, s=1, cmap='jet')
     #plt.colorbar(sc)
+    ax[i, j].set_facecolor('k')
+    print "sum_of_z_values:",np.sum(z)
+
+def lineplotdata(x, y, z, i, j, title):
+    ydim = y.shape
+    #ax[i, j].axvline(x[100], color='k', linewidth=0.025)
+    ax[i, j].set_ylim(0, 34)
+    ax[i, j].set_xlim(min(x), max(x))
+    ax[i, j].set_xticks([min(x), max(x)])
+    ax[i, j].set_aspect(0.009)
+    ax[i, j].set_title(title)
+    #sc = ax[i, j].scatter(x, y, c=z, vmin=0, vmax=1, linewidth=0.01, s=1, cmap='binary')
+    #sc = ax[i, j].scatter(x, y, c=z, vmin=0, vmax=1, linewidth=0.01, s=0.2)
+    print "min",np.min(z)
+    print "max",np.max(z)
+    #z2 = cm.jet((z-np.min(z))/(np.max(z)-np.min(z)))
+    z2 = cm.jet(z)
+    zsum = np.sum(z, axis=0)
+    print np.max(zsum)
+    for l in range(0, y.shape[1]):
+        #if zsum[l] >= np.max(zsum)*0.30:
+        if zsum[l] >= 30:
+            y2 = y[:, l]
+            z3 = z2[:, l, :]
+            for k in np.arange(x.shape[0]-1):
+                if z[k,l] > 0.125:
+                    ax[i, j].plot([x[k],x[k+1]], [y2[k],y2[k+1]], c=z3[k])
+    #plt.colorbar(sc)
+
     ax[i, j].set_facecolor('k')
     print "sum_of_z_values:",np.sum(z)
 
@@ -69,25 +100,25 @@ def get_normalvec(celldata, ic, bondlenlim):
 
 
 def get_z(celldata, atomlist, direction, zdata, bl):
-    z = np.zeros_like(zdata[0, :, 0, :])
+    z = np.zeros(zdata[0, :, 0, :].shape)
     if direction == "perp":
         for an in atomlist:
             nv = get_normalvec(celldata, an - 1, bl)
             for i in range(0, 3):
-                z += abs(zdata[0, :, an * 3 - 3 + i, :] * nv[i])**2
+                z += (np.abs(zdata[0, :, an * 3 - 3 + i, :] * nv[i]))**2
     elif direction == "z":
         for an in atomlist:
-            z += (abs(zdata[0, :, an * 3 - 1, :]))**2
+            z += (np.abs(zdata[0, :, an * 3 - 1, :]))**2
     elif direction == "xy":
         for an in atomlist:
-            z += (abs(zdata[0, :, an * 3 - 2, :]))**2
-            z += (abs(zdata[0, :, an * 3 - 3, :]))**2
+            z += (np.abs(zdata[0, :, an * 3 - 2, :]))**2
+            z += (np.abs(zdata[0, :, an * 3 - 3, :]))**2
     elif direction == "para":
         for an in atomlist:
             nv = get_normalvec(celldata, an - 1, bl)
             for i in range(0, 3):
-                z -= abs(zdata[0, :, an * 3 - 3 + i, :] * nv[i])**2
-                z += (abs(zdata[0, :, an * 3 - 3 + i, :]))**2
+                z -= np.real_if_close((np.abs(zdata[0, :, an * 3 - 3 + i, :] * nv[i]))**2, 0.1)
+                z += np.real_if_close(((np.abs(zdata[0, :, an * 3 - 3 + i, :])))**2, 0.1)
     return z
 
 
@@ -113,16 +144,20 @@ def caserun(xdata, ydata, zdata, celldata, M, K, title, bondlenlim):
         atomlist = [13, 20, 21, 28]
         z = get_z(celldata, atomlist, 'xy', zdata, bondlenlim)
     elif title == "beta_Nperp":
-        atomlist = [10, 14]
+        #atomlist = [10, 14]
+        atomlist = [19, 20, 27, 28]
         z = get_z(celldata, atomlist, 'perp', zdata, bondlenlim)
     elif title == "beta_Npara":
-        atomlist = [10, 14]
+        #atomlist = [10, 14]
+        atomlist = [19, 20, 27, 28]
         z = get_z(celldata, atomlist, 'para', zdata, bondlenlim)
     elif title == "beta_Nperp_II":
-        atomlist = [7, 8, 9, 11, 12, 13]
+        #atomlist = [7, 8, 9, 11, 12, 13]
+        atomlist = [13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26]
         z = get_z(celldata, atomlist, 'perp', zdata, bondlenlim)
     elif title == "beta_Npara_II":
-        atomlist = [7, 8, 9, 11, 12, 13]
+        #atomlist = [7, 8, 9, 11, 12, 13]
+        atomlist = [13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26]
         z = get_z(celldata, atomlist, 'para', zdata, bondlenlim)
     elif title == "beta_Nz":
         atomlist = [10, 14]
@@ -136,8 +171,12 @@ def caserun(xdata, ydata, zdata, celldata, M, K, title, bondlenlim):
     x3 = np.reshape(x2, (x2.shape[0]*x2.shape[1]))
     y2 = np.reshape(y, (y.shape[0]*y.shape[1]))
     z2 = np.reshape(z, (z.shape[0]*z.shape[1]))
+    #print x.shape
+    print y.shape
+    #print x2.shape
 
-    plotdata(x3, y2, z2, M, K, title)
+    scatterplotdata(x3, y2, z2, M, K, title) # for scatter plot
+    #lineplotdata(x, y, z, M, K, title)     # for line plot
 
 
 def get_files(bfile, pfile):
@@ -152,24 +191,26 @@ def run():
     bondlenlim = 2.0
 
     cbfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/band.hdf5"
-    cpfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/phonopy.yaml"
+    cpfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/primitive.yaml"
     cxdata, cydata, czdata, ccelldata = get_files(cbfile, cpfile)
     caserun(cxdata, cydata, czdata, ccelldata, 0, 0, "alpha_Nperp", bondlenlim)
-    caserun(cxdata, cydata, czdata, ccelldata,  0, 1, "alpha_Npara", bondlenlim)
+    #caserun(cxdata, cydata, czdata, ccelldata,  0, 1, "alpha_Npara", bondlenlim)
     #caserun(cxdata, cydata, czdata, ccelldata, 0, 2, "alpha_Nz", bondlenlim)
     #caserun(cxdata, cydata, czdata, ccelldata, 0, 3, "alpha_Nxy", bondlenlim)
-    caserun(cxdata, cydata, czdata, ccelldata, 0, 2, "alpha_Nperp_II", bondlenlim)
-    caserun(cxdata, cydata, czdata, ccelldata, 0, 3, "alpha_Npara_II", bondlenlim)
+    #caserun(cxdata, cydata, czdata, ccelldata, 0, 1, "alpha_Nperp_II", bondlenlim)
+    #caserun(cxdata, cydata, czdata, ccelldata, 0, 3, "alpha_Npara_II", bondlenlim)
 
-    sbfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/band.hdf5"
-    spfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/primitive.yaml"
+    #sbfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/band.hdf5"
+    #spfile = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk/primitive.yaml"
+    sbfile = "/home/kazu/bsi3n4_m/phonopy_doubled_334/band.hdf5"
+    spfile = "/home/kazu/bsi3n4_m/phonopy_doubled_334/primitive.yaml"
     sxdata, sydata, szdata, scelldata = get_files(sbfile, spfile)
     caserun(sxdata, sydata, szdata, scelldata, 1, 0, "beta_Nperp", bondlenlim)
-    caserun(sxdata, sydata, szdata, scelldata, 1, 1, "beta_Npara", bondlenlim)
+    #caserun(sxdata, sydata, szdata, scelldata, 1, 1, "beta_Npara", bondlenlim)
     #caserun(sxdata, sydata, szdata, scelldata, 1, 2, "beta_Nz", bondlenlim)
     #caserun(sxdata, sydata, szdata, scelldata, 1, 3, "beta_Nxy", bondlenlim)
-    caserun(sxdata, sydata, szdata, scelldata, 1, 2, "beta_Nperp_II", bondlenlim)
-    caserun(sxdata, sydata, szdata, scelldata, 1, 3, "beta_Npara_II", bondlenlim)
+    #caserun(sxdata, sydata, szdata, scelldata, 1, 1, "beta_Nperp_II", bondlenlim)
+    #caserun(sxdata, sydata, szdata, scelldata, 1, 3, "beta_Npara_II", bondlenlim)
     #plt.savefig("band-alpha-beta-gamma2.eps")
 
 run()
