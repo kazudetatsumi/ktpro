51a7965fe70183b3ef48a7d67b88d1560c900ea3
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-06-21 19:13:53 +0900

    gv_plot3d

--- gv_plot3d.py
+++ gv_plot3d.py
@@ -16,15 +16,14 @@ def parse_mesh(filename,  max_freq):
     f = h5py.File(filename, 'r')
     gv = f["group_velocity"]
     #gvdata = gv[:, :, 2]**2 / (gv[:, :, 0]**2 + gv[:, :, 1]**2 + gv[:, :, 2]**2)
-    #gvdata = ((gv[:, :, 0])**2 + (gv[:, :, 1])**2)**0.5
+    #gvdata = (gv[:, :, 0])**2 + (gv[:, :, 1])**2
     gvdata = gv[:, :, 2]**2
     #gvdata = abs(gv[:, :, 2])
     omega = f["frequency"][:, :]
     qp = f["qpoint"][:, :]
+    mesh = f["mesh"]
     qp[qp < 0] += 1
     dim = omega.shape
-    #for i in range(0, dim[0]):
-    #    print qp[i, :]
     gvdata_ave = np.zeros(dim[0])
     for i in range(0, dim[0]):
         num_ele = 0
@@ -33,51 +32,98 @@ def parse_mesh(filename,  max_freq):
             if omega[i, j] < max_freq:
                 num_ele += 1
                 sum_gvdata += gvdata[i, j]
-        #gvdata_ave[i] = sum_gvdata/num_ele
         gvdata_ave[i] = sum_gvdata
     gvdata_ave[0] = 0
 
-    return qp, gvdata_ave, omega[:, 0]
+    return mesh, qp, gvdata_ave, omega[:, 0]
 
 
-
-
-def getXYZD(qx, qy, qz, ani, n):
+def getXYZD(qx, qy, qz, gvdata_ave, n):
     xlin = np.linspace(min(qx), max(qx), n[0])
     ylin = np.linspace(min(qy), max(qy), n[1])
     zlin = np.linspace(min(qz), max(qz), n[2])
     X, Y, Z = np.meshgrid(xlin, ylin, zlin)
-    D = griddata((qx, qy, qz), ani, (X, Y, Z), method='linear')
+    D = griddata((qx, qy, qz), gvdata_ave, (X, Y, Z), method='linear')
 
     D2 = np.transpose(D, (2, 1, 0))             # The output format is adjusted to the CHGCAR file.
     D3 = D2.reshape(n[0]*n[1]*n[2] / 5, 5)      # The transposing is nescessary for it. 
     return X, Y, Z, D3
 
 
+def getXYZ(x, y, gvdata_sum, n):
+    xlin = np.linspace(min(x), max(x), n[0]*10)
+    ylin = np.linspace(min(y), max(y), n[1]*10)
+    X, Y = np.meshgrid(xlin, ylin)
+    Z = griddata((x, y), gvdata_sum, (X, Y), method='linear')
+
+    return X, Y, Z
+
+
 def make_diff(file1, file2, max_freq, n, rlat1, rlat2):
-    qp1, gvdata_ave1, omega1 = parse_mesh(file1, max_freq)
-    qp2, gvdata_ave2, omega2 = parse_mesh(file2, max_freq)
+    mesh1, qp1, gvdata_ave1, omega1 = parse_mesh(file1, max_freq)
+    mesh2, qp2, gvdata_ave2, omega2 = parse_mesh(file2, max_freq)
     X1, Y1, Z1, D1 = getXYZD(qp1[:, 0], qp1[:, 1], qp1[:, 2], gvdata_ave1, n)
     X2, Y2, Z2, D2 = getXYZD(qp2[:, 0], qp2[:, 1], qp2[:, 2], gvdata_ave2, n)
     diff = D2 * np.linalg.det(rlat2) - D1 * np.linalg.det(rlat1)
     return diff
 
+
+def make_diffonbasalp(file1, file2, max_freq, n, rlat1, rlat2):
+    mesh1, qp1, gvdata_ave1, omega1 = parse_mesh(file1, max_freq)
+    mesh2, qp2, gvdata_ave2, omega2 = parse_mesh(file2, max_freq)
+    print qp1.shape
+    #diff = gvdata_ave2 * np.linalg.det(rlat2) - gvdata_ave1 * np.linalg.det(rlat1)
+    diff = gvdata_ave2 * np.linalg.det(rlat2)
+    #diff = diff.reshape(mesh1)
+    #diff = np.sum(diff, axis=2)
+    #diff = diff.reshape(mesh1[0]*mesh1[1])
+    #car = np.matmul(qp1, rlat1)
+    #carx = car[:, 0].reshape(mesh1)
+    #carx = carx[:, :, 0]
+    #carx = carx.reshape(mesh1[0]*mesh1[1])
+    #cary = car[:, 1].reshape(mesh1)
+    #cary = cary[:, :, 0]
+    #cary = cary.reshape(mesh1[0]*mesh1[1])
+    #X, Y, Z = getXYZ(carx, cary, diff, n)
+    #plt.pcolor(X,Y,Z,cmap=cm.rainbow)
+    k = 0
+    gvsum = np.zeros((mesh1[0]*mesh1[1]))
+    x = np.zeros((mesh1[0]*mesh1[1]))
+    y = np.zeros((mesh1[0]*mesh1[1]))
+    for i in range(0, mesh1[0]):
+        for j in range(0, mesh1[1]):
+            x[k] = i * 0.1
+            y[k] = j * 0.1
+            for l in range(0, mesh1[0]*mesh1[1]*mesh1[2]):
+                if x[k] == qp1[l, 0] and y[k] == qp1[l, 1]:
+                    gvsum[k] += diff[l]
+            k += 1
+    xy = np.zeros((mesh1[0]*mesh1[1], 2))
+    xy[:, 0] = x
+    xy[:, 1] = y
+    carxy = np.matmul(xy, rlat1[0:2, 0:2])
+    #X, Y, Z = getXYZ(carxy[:,0], carxy[:,1], gvsum, n)
+    X, Y, Z = getXYZ(x, y, gvsum, n)
+    #plt.pcolor(X,Y,Z,cmap=cm.rainbow)
+    plt.contour(X, Y, Z)
+
+
+
+    
+
+
 def run():
     cdir = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift"
     sdir = "/home/kazu/bsi3n4_m/phono3py_113_fc2_338_sym_monk_shift"
     ssdir = "/home/kazu/bsi3n4_m/phonopy_doubled_334"
-    max_freq = 15
-    cn = np.array([20, 20, 28])
+    max_freq = 5
+    cn = np.array([30, 30, 42])
     sn = np.array([20, 20, 52])
     ssn = np.array([20, 20, 28])
     crlat = np.array([[0.128074059, 0.073943593, 0.000000000], [0.000000000,  0.147887185,  0.000000000], [0.000000000, 0.000000000, 0.1767063800]])
     srlat = np.array([[0.130517175, 0.075354126, 0.000000000], [0.000000000, 0.150708252, 0.00000000000], [0.000000000, 0.000000000, 0.3417394180]])
     ssrlat = np.array([[0.130556943, 0.075377086, 0.000000000], [0.000000000, 0.150754172, 0.00000000000], [0.000000000, 0.000000000, 0.1709573530]])
-    #cdlat = np.array([[7.807982394, 0.000000000, 0.000000000],[-3.903991197, 6.761911106, 0.0000000000],[0.000000000, 0.000000000, 5.6591052420]])
-    #print 1/np.linalg.det(crlat)
-    #print 1/np.linalg.det(cdlat)
-    y_s = 0.0
-    c = cdir + "/mesh.hdf5"
+    c = cdir + "/mesh.hdf5.org"
     s = sdir + "/mesh.hdf5"
     ss = ssdir + "/mesh.hdf5"
     #For data extraction for each phase
@@ -86,8 +132,10 @@ def run():
     #np.savetxt("array.txt", D3 * np.linalg.det(crlat), fmt="%17.11e")  #
     
     #Seek the difference between alpha and beta_doubled_cell
-    diff = make_diff(c, ss, max_freq, cn, crlat, ssrlat)
-    np.savetxt("array.txt", diff, fmt="%17.11e")  #
+    #diff = make_diff(c, ss, max_freq, cn, crlat, ssrlat)
+    make_diffonbasalp(c, ss, max_freq, cn, crlat,ssrlat)
+    #np.savetxt("array.txt", diff, fmt="%17.11e")  #
+    plt.show()
 
 
 
