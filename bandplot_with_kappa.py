66629c0412c3c2f39cbc3ff9db8e32e0790e5e76
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-08-07 18:56:46 +0900

    bandplot_with_kappa.py kappa-image, gv-image

--- bandplot_with_kappa.py
+++ bandplot_with_kappa.py
@@ -25,6 +25,12 @@ def parse_band(bfile):
     return(xdata, ydata, zdata)
 
 
+def parse_mesh(filename):
+    f = h5py.File(filename, 'r')
+    gv = f["group_velocity"]
+    qp = f["qpoint"][:, :]
+    return gv, qp
+
 
 def parse_kappa(filename,  temp):
     f = h5py.File(filename, 'r')
@@ -33,16 +39,16 @@ def parse_kappa(filename,  temp):
     kuc = f["kappa_unit_conversion"][()]
     qp = f["qpoint"][:, :]  # (gp, 3)
     print "qp",qp.shape
+    qp[qp < 0] += 1
     if filename == "/home/kazu/bsi3n4_m/phono3py_doubled_112_fc2_334_sym_monk/kappa-m141416.bz.hdf5":
         print "chk", filename,"does not contain proper gv"
         gv, qpm = parse_mesh("/home/kazu/bsi3n4_m/phono3py_doubled_112_fc2_334_sym_monk/mesh_m141416.hdf5")
         qpm[qpm < 0] += 1
         print "qpm", qpm.shape
-        print np.where((qp - qpm) > 0.01)
-        print qpm[86,:], qp[86,:]
+        print "qpm2",np.where((qp - qpm) > 0.01)
+        print "qpm3",qpm[186,:], qp[186,:]
     else:
-        gv = f["group_velocity"] #(gp, band)
-    qp[qp < 0] += 1
+        gv = f["group_velocity"] #(gp, band, 3)
     omega = f["frequency"][:, :] # (gp, band)
     gamma = f['gamma'][:] #KT, (temps, gp, band)
     cv = f['heat_capacity'][:] # (temps, gp, band)
@@ -52,7 +58,8 @@ def parse_kappa(filename,  temp):
     gamma = np.squeeze(gamma[condition, :, :])
     kappa = kuc * cv * gv[:, :, 2] * gv[:, :, 2] / (2 * gamma)  # (gp, band)
     kappa[0, 0:3] = 0
-    return(qp, kappa, omega)
+    print "kappa=",np.sum(kappa) / (mesh[0]*mesh[1]*mesh[2])
+    return(qp, kappa, gv[:, :, 2], omega)
 
 
 
@@ -66,64 +73,81 @@ def get_files(bfile, pfile):
 
 
 
-def binimage(x, y, nb):
+def binimage(x, y, k, nb):
     x = np.squeeze(x)
     y = np.squeeze(y)
     ylin = np.linspace(-1.0, 40.0, nb+1)
     nsize = y.shape
     nc = np.zeros((nsize[0], nb))
-    values = np.ones(nsize[1])
+    #values = np.ones(nsize[1])
     for i in range(0, nsize[0]):
-        nc[i, :], res2, res3 = stats.binned_statistic(y[i, :], values, "sum", ylin)
+        nc[i, :], res2, res3 = stats.binned_statistic(y[i, :], k[i,:], "sum", ylin)
 
     X, Y = np.meshgrid(x, ylin)
     nc = np.transpose(nc)
     for i in range(0, nsize[0]):
-        print "CHK", np.sum(nc[:, i])
+        print "nc_CHK", np.sum(nc[:, i])
     #plt.figure()
     #plt.pcolor(X, Y, nc, cmap=cm.gray)
     return X, Y, nc
 
 
 def oned_kappa(qp, kappa, omega):
-    qpx = qp[:,0]
-    qpy = qp[:,1]
-    qpz = qp[:,2]
-    condition = abs(qpx - 4/14) + abs(qpy - 1/14) + abs(qpz - 1/8) < 0.01
-    _k = kappa[condition,:]
-    _o = omega[condition,:]
-    print condition.shape
-    print "selected kappa",_k, _k.shape
-    print "selected omega",_o, _o.shape
+    qpx = qp[:, 0]
+    qpy = qp[:, 1]
+    qpz = qp[:, 2]
+    nsize = kappa.shape
+    onedk = np.zeros((9, nsize[1]))
+    for i in range(0, 9):
+        condition = abs(qpx - (4.0/14.0)) + abs(qpy - (1.0/14.0)) + abs(qpz - (i/16.0)) < 0.01
+        print qp[condition, :]
+        _k = kappa[condition, :]
+        _o = omega[condition, :]
+        _o = np.squeeze(_o)
+        onedk[i, :] = _k
+        for j in range(0, nsize[1]-1):
+            if _o[j] > _o[j+1]:
+                print "order error in omega values"
+    return onedk
+
 
 
 def run():
     bondlenlim = 2.0
-    nybin = 40
+    nybin = 90
     temp = 300
 
     cbfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/band_4-14_1-14.hdf5"
     cpfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/primitive.yaml"
     cxdata, cydata, czdata, ccelldata = get_files(cbfile, cpfile)
     ckfile = "/home/kazu/asi3n4/phono3py_112_fc2_334_sym_monk_shift/noiso/kappa-m141416.bz.hdf5"
-    qp1, kappa1, omega1 = parse_kappa(ckfile, temp)
-    kappa = oned_kappa(qp1, kappa1, omega1)
+    qp1, kappa1, gv1, omega1 = parse_kappa(ckfile, temp)
+    #kappa1= oned_kappa(qp1, kappa1, omega1)
+    kappa1= oned_kappa(qp1, gv1*gv1, omega1)
 
 
-    cX,cY,cnc = binimage(cxdata, cydata, nybin)
+    cX,cY,cnc = binimage(cxdata, cydata, kappa1, nybin)
     sbfile = "/home/kazu/bsi3n4_m/phonopy_doubled_334/band_4-14_1-14.hdf5"
     spfile = "/home/kazu/bsi3n4_m/phonopy_doubled_334/primitive.yaml"
     sxdata, sydata, szdata, scelldata = get_files(sbfile, spfile)
-    sX,sY,snc = binimage(sxdata, sydata, nybin)
-    im=ax[0].pcolor(cX, cY, cnc, cmap=cm.gray)
-    im=ax[1].pcolor(sX, sY, snc, cmap=cm.gray)
-    #from matplotlib.colors import Normalize
-    #norm = Normalize(vmin=0, vmax=1)
-    #from matplotlib.cm import ScalarMappable, get_cmap
-    #cmap = get_cmap("jet")
-    #mappable=ScalarMappable(norm=norm,cmap=cmap)
-    #mappable._A = []
-    #plt.colorbar(mappable)
+    skfile = "/home/kazu/bsi3n4_m/phono3py_doubled_112_fc2_334_sym_monk/kappa-m141416.bz.hdf5"
+    qp2, kappa2, gv2, omega2 = parse_kappa(skfile, temp)
+    #kappa2= oned_kappa(qp2, kappa2, omega2)
+    kappa2= oned_kappa(qp2, gv2*gv2, omega2)
+
+
+    sX,sY,snc = binimage(sxdata, sydata, kappa2, nybin)
+    maxs=np.max(snc)
+    im=ax[0].pcolor(cX, cY, cnc,vmin=0, vmax=maxs, cmap=cm.gray)
+    im=ax[1].pcolor(sX, sY, snc,vmin=0, vmax=maxs, cmap=cm.gray)
+    plt.figure()
+    from matplotlib.colors import Normalize
+    norm = Normalize(vmin=0, vmax=maxs)
+    from matplotlib.cm import ScalarMappable, get_cmap
+    cmap = get_cmap("gray")
+    mappable=ScalarMappable(norm=norm,cmap=cmap)
+    mappable._A = []
+    plt.colorbar(mappable)
 
 #THztoKayser = 33.35641
 run()
