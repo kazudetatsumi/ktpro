1df5483abf71f3e1ebb82c88958a0f0eac23faa0
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-09-13 19:39:18 +0900

    optbinwidth.py

---
+++ optbinwidth.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def getdata(f):
+    data = np.genfromtxt(f,  delimiter=',', dtype=None)
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
+    dx = 0.005
+    dy = 0.1
+    xlin = np.arange(min(x), max(x), dx)
+    nx = xlin.shape[0]
+    ylin = np.arange(min(y), max(y), dy)
+    ny = ylin.shape[0]
+    
+    karr = np.zeros((nx, ny))
+
+    for _x, _y, _z in zip(x, y, z):
+       xx =  np.where(abs(xlin - _x) < 0.0000001)
+       yy =  np.where(abs(ylin - _y) < 0.0000001)
+       karr[xx, yy] = _z
+
+    return np.sum(karr[390:400,:], axis=0)
+
+def calc_hist(cumdata, nw):
+    Nmax = cumdata.shape[0]
+    N = int((Nmax - (Nmax % nw)) / nw)  
+    k = np.zeros((N))
+    for i in range(0, N):
+        ihead = (i+1)*nw - 1
+        if i == 0:
+            k[i] = cumdata[ihead]
+        else:
+            k[i] = cumdata[ihead] - cumdata[ihead - nw]
+    return k
+        
+def calc_cost(cumdata, maxw):
+    Cn = np.zeros((maxw))
+    for i in range(2, maxw):
+       k = calc_hist(cumdata, i)
+       kave = np.average(k)
+       v = np.var(k)
+       cost = (2 * kave - v) / (i**2)
+       Cn[i] = cost
+    return Cn
+
+
+def run():
+    #minx = -1.33; maxx = 2.30; dx = 0.005; nx = int((maxx - minx)/dx) + 1
+    #miny = -5.0; maxy = 42.0; dy = 0.100; ny = int((maxy - miny)/dy) + 1
+    txtfile = "/home/kazu/data/5min_fine.txt"
+    data = getdata(txtfile)
+    maxw =  int(data.shape[0] / 2)
+    print data[0:10]
+    cumdata = np.cumsum(data)
+    print cumdata[0:10]
+
+    #k = calc_hist(cumdata, 2)
+    #kave = np.average(k)
+    #v = np.var(k)
+    #Cn = (2 * kave - v) / 2**2
+    #print Cn
+    Cn = calc_cost(cumdata, maxw)
+    print np.argmin(Cn)
+    k = calc_hist(cumdata, 22)
+
+    plt.figure(figsize=(16, 8))
+    plt.plot(k)
+    plt.figure(figsize=(16, 8))
+    plt.plot(data)
+
+run()
+plt.show()
