5954c4ef16d413258c40c9d84e90f970e9e01f5c
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-10-08 02:42:49 +0900

    optbinwidth.py with fortran tst2d

---
+++ histfort_.f90
@@ -0,0 +1,103 @@
+module histfort
+  use ISO_C_binding
+  implicit none
+  type, bind(C) :: result
+    integer(c_int) :: len0
+    integer(c_int) :: len1
+    type(c_ptr) :: arr
+  end type result
+
+contains
+
+  function hist1d(nw0, Al0, A) bind(C, name="hist1d")
+    !DEC$ ATTRIBUTES DLLEXPORT :: plus
+    integer(c_int), intent(in) :: nw0
+    integer(c_int), intent(in) :: Al0
+    real(c_double), intent(in) :: A(Al0)                         
+    type(result) :: hist1d                                  
+    real(c_double), pointer :: work_array(:)                    
+    integer N0
+    integer i, ihead
+    N0 = (Al0 - mod(Al0, nw0)) / nw0
+    allocate(work_array(N0))
+
+    do i = 1, N0
+       ihead = i*nw0 
+       if ( i == 1) then
+           work_array(i) = A(ihead)
+       else
+           work_array(i) = A(ihead) - A(ihead - nw0)
+       end if
+    end do
+
+    hist1d%len0 =  N0
+    hist1d%arr = C_loc(work_array)
+  end function hist1d
+
+
+
+  function hist2d(nw1, nw0, Al1, Al0, A) bind(C, name="hist2d")
+    !DEC$ ATTRIBUTES DLLEXPORT :: plus
+    integer(c_int), intent(in) :: nw0
+    integer(c_int), intent(in) :: nw1
+    integer(c_int), intent(in) :: Al0
+    integer(c_int), intent(in) :: Al1
+    real(c_double), intent(in) :: A(Al0, Al1)                         
+    type(result) :: hist2d                                  
+    real(c_double), pointer :: work_array(:, :)                    
+    integer N0, N1
+    integer i, ihead, j, jhead
+    N0 = (Al0 - mod(Al0, nw0)) / nw0
+    N1 = (Al1 - mod(Al1, nw1)) / nw1
+    allocate(work_array(N0, N1))
+
+    do i = 1, N0
+       ihead = i*nw0 
+       do j = 1, N1
+          jhead = j*nw1
+          if ( i == 1 .and. j == 1 ) then
+             work_array(i, j) = A(ihead, jhead)
+          else if ( j == 1 .and. i /= 1 ) then
+             work_array(i, j) = A(ihead, jhead) - A(ihead - nw0, jhead)
+          else if ( i == 1 .and. j /= 1 ) then
+             work_array(i, j) = A(ihead, jhead) - A(ihead, jhead - nw1)
+          else
+             work_array(i, j) = A(ihead, jhead) - A(ihead - nw0, jhead) - A(ihead, jhead - nw1) + A(ihead - nw0, jhead - nw1)
+          end if
+       end do
+    end do
+    print *, "do u understand the invesion of the order of matrix shape?"
+
+    hist2d%len0 =  N1
+    hist2d%len1 =  N0
+    hist2d%arr = C_loc(work_array)
+  end function hist2d
+
+
+  subroutine delete_array(arr_length, array) bind(C, name="delete_array")
+    !DEC$ ATTRIBUTES DLLEXPORT :: delete_array
+    integer(c_int), intent(in) :: arr_length
+    type(c_ptr), value :: array
+    real(c_double), pointer :: work_array(:)
+
+    call C_F_pointer(array, work_array, [arr_length])
+    deallocate(work_array)
+    array = C_NULL_PTR
+    print *, "Is work_array freed ?, work_array: ",work_array
+  end subroutine delete_array
+
+
+  subroutine delete_array2(arr_length0, arr_length1, array) bind(C, name="delete_array2")
+    !DEC$ ATTRIBUTES DLLEXPORT :: delete_array
+    integer(c_int), intent(in) :: arr_length0
+    integer(c_int), intent(in) :: arr_length1
+    type(c_ptr), value :: array
+    real(c_double), pointer :: work_array(:,:)
+
+    call C_F_pointer(array, work_array, [arr_length0, arr_length1])
+    deallocate(work_array)
+    array = C_NULL_PTR
+    print *, "Is work_array freed ?, work_array: ",work_array
+  end subroutine delete_array2
+
+end module histfort
--- optbinwidth.py
+++ optbinwidth.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import h5py
 import ctypes
-lib = ctypes.CDLL("./histfort.so")
+lib = ctypes.CDLL("./histfort_.so")
 
 
 def get2ddata(f, xi, xf, yi, yf):
@@ -96,6 +96,32 @@ def calc_hist1d_f90(A, nw0, condition):
     for i in range(0, result_len0):
         kcond[i] = np.sum(condition[i*nw0:(i+1)*nw0])
     return result_vec, result_len0, kcond
+
+
+def calc_hist2d_f90(A, nw0, nw1,  condition):
+
+    class result(ctypes.Structure):
+        _fields_ = [("len0", ctypes.c_int), ("len1", ctypes.c_int), ("arr", ctypes.POINTER(ctypes.c_double))]
+
+    lib.hist2d.restype = result
+    lib.hist2d.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int),
+                           ctypes.POINTER(ctypes.c_int),  ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=2)]
+
+    Nmax0 = A.shape[0]
+    Nmax1 = A.shape[1]
+
+    result = lib.hist2d(ctypes.byref(ctypes.c_int(nw0)), ctypes.byref(ctypes.c_int(nw1)),
+                        ctypes.byref(ctypes.c_int(Nmax0)), ctypes.byref(ctypes.c_int(Nmax1)), A)
+    result_len0 = result.len0
+    result_len1 = result.len1
+    result_vec = np.ctypeslib.as_array(result.arr, shape=(result_len0, result_len1, ))
+    print result_vec
+
+    kcond = np.zeros((result_len0, result_len1))
+    for i in range(0, result_len0):
+        for j in range(0, result_len1):
+            kcond[i, j] = np.sum(condition[i*nw0:(i+1)*nw0, j*nw1:(j+1)*nw1])
+    return result_vec, result_len0, result_len1, kcond
 
 
 def calc_hist2d(A, nw0, nw1, condition):
@@ -408,6 +434,31 @@ def calc_hist4d_f90(A, data, nw0, nw1, nw2, nw3, condition):
     return k, kcond
 
 
+def calc_cost1d(cumdata, maxw, condition, fflag):
+    Cn = np.zeros((maxw))
+    kaves = np.zeros((maxw))
+    deltas = np.zeros((maxw))
+    for i in range(1, maxw):
+       if fflag == 1:
+          k, klen, kcond = calc_hist1d_f90(cumdata, i, condition) 
+       else:
+          k,  kcond = calc_hist1d(cumdata, i, condition) 
+       kave = np.average(k)
+       v = np.var(k)
+       cost = (2 * kave - v) / (i**2)
+       Cn[i] = cost
+       kaves[i] = kave
+       deltas[i] = (i*1.0)
+
+       if fflag == 1:
+          lib.delete_array.restype = None
+          lib.delete_array.argtypes = [ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=1)]
+          lib.delete_array(ctypes.byref(ctypes.c_int(klen)), k)
+
+    deltas[0] = 1.0
+    return Cn, kaves, deltas
+
+
 def calc_cost2d(A, maxw, condition):
     Cn = np.zeros((maxw))
     kaves = np.zeros((maxw))
@@ -430,6 +481,29 @@ def calc_cost2d(A, maxw, condition):
           #kf = k.flatten()                            
           #kave = np.average(kf)/fracdata
           #v =  np.sum((kf - kave)**2)/(kf.shape[0]*1.0*fracdata)
+          cost = (2 * kave - v) / ((i*j)**2*1.0)
+          print "cost with (i, j) = ", i, j, ":", cost, "kave", kave, "v", v
+          Cn[i, j] = cost
+          kaves[i, j] = kave
+          deltas[i, j] = (i*j*1.0)
+    return Cn, kaves, deltas
+
+
+def calc_cost2d_f90(A, maxw, condition):
+    Cn = np.zeros((maxw))
+    kaves = np.zeros((maxw))
+    deltas = np.zeros((maxw))
+    for i in range(1, maxw[0]):
+       for j in range(1, maxw[1]):
+          k, klen0, klen1, kcond = calc_hist2d_f90(A, i, j, condition)
+          knonzero = np.extract(np.max(kcond) == kcond, k)
+          if i == 1 and j ==1:
+             print "shape of k matrix with zero elements",k.shape
+             print "total number of k is", k.shape[0]*k.shape[1]
+             print "number of nonzero k is",knonzero.shape 
+          kave = np.average(knonzero)
+          v = np.var(knonzero)
+          
           cost = (2 * kave - v) / ((i*j)**2*1.0)
           print "cost with (i, j) = ", i, j, ":", cost, "kave", kave, "v", v
           Cn[i, j] = cost
@@ -815,21 +889,187 @@ def run_tst1d():
     return k, klen
 
 
+def run1d():
+    datafile = "/home/kazu/cscl/phonopy_222/m200200200/data3_100000000.hdf5"
+    f = h5py.File(datafile)
+    fflag = 1
+    data = f["data3"][:] # nqx, nqy, nqz, nomega
+    data = data[0, 0, 0, :]*1.0
+    n = np.sum(data)*1.0
+    print "n=", n
+    condition = np.ones(data.shape, dtype=bool)
+    maxw =  int(data.shape[0] / 2)
+    cumdata = np.cumsum(data)
+
+    Cn, kaves, deltas = calc_cost1d(cumdata, maxw, condition, fflag)
+    Cn = Cn / (n**2)   # This is according to the Cn in NeCo(2007)
+    print "argmin(Cn)",np.argmin(Cn)
+
+    m = 0.10*n
+
+    ex = (1/m - 1/n) * kaves / (deltas**2*n) 
+    ex[0] = 0.0
+
+    Cm = ex + Cn
+
+    print "argmin(Cm)",np.argmin(Cm)
+
+    if fflag == 1:
+        k, klen, kcond = calc_hist1d_f90(cumdata, 3, condition) 
+    else:
+        k, cond = calc_hist1d(cumdata, 3, condition)
+
+    plt.figure(figsize=(16, 8)) 
+    plt.plot(k)
+    plt.figure(figsize=(16, 8)) 
+    #plt.plot(data)
+    plt.plot(Cm)
+
+    if fflag == 1:
+       lib.delete_array.restype = None
+       lib.delete_array.argtypes = [ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=1)]
+       lib.delete_array(ctypes.byref(ctypes.c_int(klen)), k)
+
+
+def run_tst2d():
+    datafile = "/home/kazu/cscl/phonopy_222/m200200200/data3_100000000.hdf5"
+    f = h5py.File(datafile)
+    data = f["data3"][:] # nqx, nqy, nqz, nomega
+    data = data[0, :, 0, :]*1.0
+    condition = np.ones(data.shape, dtype=bool)
+    
+    A = np.cumsum(np.cumsum(data, axis=0), axis=1)
+
+    k, klen0, klen1,  kcond = calc_hist2d_f90(A, 2, 2, condition) 
+    #k,   kcond = calc_hist2d(A, 2, 2, condition) 
+
+
+    plt.figure(figsize=(8, 16))
+    plt.pcolor(np.transpose(k), vmax=np.max(k), cmap='jet')
+    mappable = make_mappable(np.max(k))
+    plt.colorbar(mappable)
+    plt.figure(figsize=(8, 16))
+    plt.pcolor(np.transpose(data), vmax=np.max(data), cmap='jet')
+
+    mappable = make_mappable(np.max(data))
+    plt.colorbar(mappable)
+
+    lib.delete_array2.restype = None
+    lib.delete_array2.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=2)]
+    lib.delete_array2(ctypes.byref(ctypes.c_int(klen0)), ctypes.byref(ctypes.c_int(klen1)), k)
+
+
+def run2d_f90():
+    datafile = "/home/kazu/cscl/phonopy_222/m200200200/data3_100000000.hdf5"
+    f = h5py.File(datafile)
+    data = f["data3"][:] # nqx, nqy, nqz, nomega
+    data = data[0, :, 0, :]*1.0
+    condition = np.ones(data.shape, dtype=bool)
+    n = np.sum(data)*1.0
+    print "n=", n
+    maxxwidth = np.min(np.sum(condition, axis=0)) / 2
+    maxywidth = np.min(np.sum(condition, axis=1)) / 2
+    maxw = np.array([maxxwidth, maxywidth])
+    print maxw
+    A = np.cumsum(np.cumsum(data, axis=0), axis=1)
+
+    Cn, kaves, deltas = calc_cost2d_f90(A, maxw, condition)
+
+    Cn = Cn / (n**2)   # This is according to the Cn in NeCo(2007)
+    print "argmin(Cn)",np.argmin(Cn)
+    k, klen, kcond = calc_hist1d_f90(cumdata, 3, condition) 
+
+    m = 1.0*n
+
+    ex = (1/m - 1/n) * kaves / (deltas**2*n) 
+    ex[0] = 0.0
+
+    Cm = ex + Cn
+
+    print "argmin(Cm)",np.argmin(Cm)
+
+
+    #plt.figure(figsize=(16, 8)) 
+    #plt.plot(k)
+    #plt.figure(figsize=(16, 8)) 
+    #plt.plot(data)
+    #plt.plot(Cm)
+
+    lib.delete_array.restype = None
+    lib.delete_array.argtypes = [ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=1)]
+    lib.delete_array(ctypes.byref(ctypes.c_int(klen)), k)
+
+
+def runex():
+    txtfile = "/home/kazu/data/20min_fine.txt"
+    #xi = 119
+    xi = 119
+    xf = 222
+    yi = 25
+    #yf = 340
+    yf = 530
+    
+    data, condition = get2ddata(txtfile, xi, xf, yi, yf)
+    n = np.sum(data)*1.0
+    print "n=", n
+
+
+    maxxwidth = np.min(np.sum(condition, axis=0)) / 2
+    maxywidth = np.min(np.sum(condition, axis=1)) / 2
+    
+    #maxw = np.array([int(data.shape[0] / 2), int(data.shape[1]) / 2])
+    maxw = np.array([maxxwidth, maxywidth])
+    print maxw
+    A = np.cumsum(np.cumsum(data, axis=0), axis=1)
+
+    Cn, kaves, delstas = calc_cost2d(A, maxw, condition)
+    Cn = Cn / (n**2)   # This is according to the Cn in NeCo(2007)
+
+    m = 15.0*n
+
+    ex = (1/m - 1/n) * kaves / (delstas**2*n) 
+    ex[0, :] = 0.0
+    ex[:, 0] = 0.0
+
+    #print "ex, size",ex.shape
+    #print "ex", ex[0:5, 0:5]
+    #print Cn[0:5, 0:5]
+    Cm = ex + Cn
+
+    print "opt bin index", np.unravel_index(np.argmin(Cm, axis=None), Cm.shape)
+    opt_indx = np.unravel_index(np.argmin(Cm, axis=None), Cm.shape)
+    k, kcond = calc_hist2d(A, opt_indx[0], opt_indx[1], condition) 
+    #plt.figure(figsize=(16, 8))
+    #plt.plot(Cn[10,:])
+    #plt.plot(Cm[10,:])
+
+    plt.figure(figsize=(16, 8))
+    #plt.pcolor(np.transpose(k), vmax=np.max(k), cmap='jet')
+    plt.pcolor(np.transpose(k), vmax=np.max(k), cmap='jet')
+    mappable = make_mappable(np.max(k))
+    plt.colorbar(mappable)
+    plt.figure(figsize=(16, 8))
+    plt.pcolor(np.transpose(data), vmax=np.max(data)/1000, cmap='jet')
+
+    mappable = make_mappable(np.max(data)/1000)
+    plt.colorbar(mappable)
 #run_tst()
+run_tst2d()
 #run2d()
+#run2d_f90
 #runex()
 #run_simu()
 #run_simu3d()
-
+#run1d()
 #run_tst()
 #run2d()
 #runex()
 #run_simu()
 #run_simu4d()
-k, klen = run_tst1d()
-lib.delete_array.restype = None
-lib.delete_array.argtypes = [ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=1)]
-lib.delete_array(ctypes.byref(ctypes.c_int(klen)), k)
-#plt.show()
+#k, klen = run_tst1d()
+#lib.delete_array.restype = None
+#lib.delete_array.argtypes = [ctypes.POINTER(ctypes.c_int), np.ctypeslib.ndpointer(dtype=np.float64, ndim=1)]
+#lib.delete_array(ctypes.byref(ctypes.c_int(klen)), k)
+plt.show()
 
 
