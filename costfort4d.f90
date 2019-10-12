275460732b764acf4a359e59645e1dc1aacbb5d5
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2019-10-12 15:08:48 +0900

    optbinwidth4d_wholefort.py cumsum routine in fortran

--- costfort4d.f90
+++ costfort4d.f90
@@ -13,7 +13,8 @@ module costfort4d
 
 contains
 
-  function cost4d(maxw3, maxw2, maxw1, maxw0, Al3, Al2, Al1, Al0, A, D, CD) bind(C, name="cost4d")
+  !function cost4d(maxw3, maxw2, maxw1, maxw0, Al3, Al2, Al1, Al0, AA, D, CD) bind(C, name="cost4d")
+  function cost4d(maxw3, maxw2, maxw1, maxw0, Al3, Al2, Al1, Al0, D, CD) bind(C, name="cost4d")
     !DEC$ ATTRIBUTES DLLEXPORT :: cost4d
     integer(c_int), intent(in) :: maxw0
     integer(c_int), intent(in) :: maxw1
@@ -23,12 +24,13 @@ contains
     integer(c_int), intent(in) :: Al1
     integer(c_int), intent(in) :: Al2
     integer(c_int), intent(in) :: Al3
-    real(c_double), intent(in) :: A(Al0, Al1, Al2, Al3)                         
+    !real(c_double), intent(in) :: AA(Al0, Al1, Al2, Al3)                         
     real(c_double), intent(in) :: D(Al0, Al1, Al2, Al3)                         
-    real(c_double), intent(in) :: CD(Al0, Al1, Al2, Al3)                         
+    integer(c_int), intent(in) :: CD(Al0, Al1, Al2, Al3)                         
     type(result) :: cost4d                                  
     !real(c_double), pointer :: k(:,:,:,:)                    
     double precision, allocatable :: k(:,:,:,:)                    
+    integer, allocatable :: kcond(:,:,:,:)                    
     real(c_double), pointer :: Cn(:,:,:,:)                    
     real(c_double), pointer :: kaves(:,:,:,:)                    
     real(c_double), pointer :: deltas(:,:,:,:)                    
@@ -36,7 +38,10 @@ contains
     integer N0, N1, N2, N3
     integer i, ihead, j, jhead, h, hhead, l, lhead
     double precision kave, v
+    double precision A(Al0, Al1, Al2, Al3)
     !allocate(k(Al0, Al1, Al2, Al3))
+    A = cumsum4d(cumsum4d(cumsum4d(cumsum4d(D, 1),2),3),4)
+    
     allocate(Cn(maxw0, maxw1, maxw2, maxw3))
     allocate(kaves(maxw0, maxw1, maxw2, maxw3))
     allocate(deltas(maxw0, maxw1, maxw2, maxw3))
@@ -52,8 +57,10 @@ contains
     do nw3 = 1, maxw3
     N3 = (Al3 - mod(Al3, nw3)) / nw3
     allocate(k(N0, N1, N2, N3))
+    allocate(kcond(N0, N1, N2, N3))
        if (nw0*nw1*nw2*nw3 == 1) then
        k = D
+       kcond = CD
        else
        !$omp parallel do
        do i = 1, N0
@@ -151,6 +158,7 @@ contains
        deltas(nw0, nw1, nw2, nw3) = real(nw0*nw1*nw2*nw3)
        kaves(nw0, nw1, nw2, nw3) = kave
        deallocate(k)
+       deallocate(kcond)
     end do
     end do
     end do
@@ -167,6 +175,7 @@ contains
     cost4d%kavearr = C_loc(kaves(1:maxw0, 1:maxw1, 1:maxw2, 1:maxw3))
     cost4d%darr = C_loc(deltas(1:maxw0, 1:maxw1, 1:maxw2, 1:maxw3))
   end function cost4d
+
 
   subroutine delete_array(arr_length3, arr_length2, arr_length1, arr_length0, carray, karray, darray) bind(C, name="delete_array")
     !DEC$ ATTRIBUTES DLLEXPORT :: delete_array
@@ -191,5 +200,63 @@ contains
     karray = C_NULL_PTR
     darray = C_NULL_PTR
   end subroutine delete_array
+
+  function cumsum4d(d, axis)
+      integer, intent(in) :: axis
+      double precision, intent(in) :: d(:,:,:,:)
+      double precision :: cumsum4d(size(d,1), size(d,2), size(d,3), size(d,4))
+      integer :: i, j, k, l 
+      cumsum4d = d
+
+      if (axis == 4) then
+         !$omp parallel do
+         do i = 1, size(d,1)
+         do j = 1, size(d,2)
+         do k = 1, size(d,3)
+         do l = 1, size(d,4) - 1
+            cumsum4d(i, j, k, l + 1) = cumsum4d(i, j, k, l) + d(i, j, k, l + 1)
+         end do
+         end do
+         end do
+         end do
+      else if (axis == 3) then
+         !$omp parallel do
+         do i = 1, size(d,1)
+         do j = 1, size(d,2)
+         do l = 1, size(d,4)
+         do k = 1, size(d,3) - 1
+            cumsum4d(i, j, k + 1, l) = cumsum4d(i, j, k, l) + d(i, j, k + 1, l)
+         end do
+         end do
+         end do
+         end do
+      else if (axis == 2) then
+         !$omp parallel do
+         do i = 1, size(d,1)
+         do k = 1, size(d,3)
+         do l = 1, size(d,4)
+         do j = 1, size(d,2) - 1
+            cumsum4d(i, j + 1, k, l) = cumsum4d(i, j, k, l) + d(i, j + 1, k, l)
+         end do
+         end do
+         end do
+         end do
+      else if (axis == 1) then
+         !$omp parallel do
+         do j = 1, size(d,2) 
+         do k = 1, size(d,3)
+         do l = 1, size(d,4)
+         do i = 1, size(d,1) - 1
+            cumsum4d(i + 1, j, k, l) = cumsum4d(i, j, k, l) + d(i + 1, j, k, l)
+         end do
+         end do
+         end do
+         end do
+      end if
+   end function cumsum4d       
+
+
+
+
 
 end module costfort4d
--- optbinwidth4d_wholefort.py
+++ optbinwidth4d_wholefort.py
@@ -6,7 +6,8 @@ import h5py
 import ctypes
 lib = ctypes.CDLL("./costfort4d.so")
 
-def calc_cost4d_f90(A, maxw, data, condition):
+#def calc_cost4d_f90(A, maxw, data, condition):
+def calc_cost4d_f90(maxw, data, condition):
     class result(ctypes.Structure):
         _fields_ =[("len0", ctypes.c_int), ("len1", ctypes.c_int), ("len2", ctypes.c_int), ("len3", ctypes.c_int),
                   ("arr", ctypes.POINTER(ctypes.c_double)), ("kavearr", ctypes.POINTER(ctypes.c_double)), ("darr", ctypes.POINTER(ctypes.c_double))]
@@ -14,19 +15,24 @@ def calc_cost4d_f90(A, maxw, data, condition):
     lib.cost4d.restype = result
     lib.cost4d.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int),
                            ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int),
-                           np.ctypeslib.ndpointer(dtype=np.float64, ndim=4), np.ctypeslib.ndpointer(dtype=np.float64, ndim=4), np.ctypeslib.ndpointer(dtype=np.bool, ndim=4)]
+                           #np.ctypeslib.ndpointer(dtype=np.float64, ndim=4),
+                           np.ctypeslib.ndpointer(dtype=np.float64, ndim=4),
+                           np.ctypeslib.ndpointer(dtype=np.int32, ndim=4)]
 
-    Nmax0 = A.shape[0]
-    Nmax1 = A.shape[1]
-    Nmax2 = A.shape[2]
-    Nmax3 = A.shape[3]
+    Nmax0 = data.shape[0]
+    Nmax1 = data.shape[1]
+    Nmax2 = data.shape[2]
+    Nmax3 = data.shape[3]
 
+    #result = lib.cost4d(ctypes.byref(ctypes.c_int(maxw[0])), ctypes.byref(ctypes.c_int(maxw[1])), ctypes.byref(ctypes.c_int(maxw[2])), ctypes.byref(ctypes.c_int(maxw[3])),
+    #                    ctypes.byref(ctypes.c_int(Nmax0)), ctypes.byref(ctypes.c_int(Nmax1)), ctypes.byref(ctypes.c_int(Nmax2)), ctypes.byref(ctypes.c_int(Nmax3)), A, data, condition)
     result = lib.cost4d(ctypes.byref(ctypes.c_int(maxw[0])), ctypes.byref(ctypes.c_int(maxw[1])), ctypes.byref(ctypes.c_int(maxw[2])), ctypes.byref(ctypes.c_int(maxw[3])),
-                        ctypes.byref(ctypes.c_int(Nmax0)), ctypes.byref(ctypes.c_int(Nmax1)), ctypes.byref(ctypes.c_int(Nmax2)), ctypes.byref(ctypes.c_int(Nmax3)), A, data, condition)
+                        ctypes.byref(ctypes.c_int(Nmax0)), ctypes.byref(ctypes.c_int(Nmax1)), ctypes.byref(ctypes.c_int(Nmax2)), ctypes.byref(ctypes.c_int(Nmax3)),  data, condition)
     result_len0 = result.len0
     result_len1 = result.len1
     result_len2 = result.len2
     result_len3 = result.len3
+    Cn = np.ctypeslib.as_array(result.arr, shape=(result_len0, result_len1, result_len2, result_len3, ))
     Cn = np.ctypeslib.as_array(result.arr, shape=(result_len0, result_len1, result_len2, result_len3, ))
     kaves = np.ctypeslib.as_array(result.kavearr, shape=(result_len0, result_len1, result_len2, result_len3, ))
     deltas = np.ctypeslib.as_array(result.darr, shape=(result_len0, result_len1, result_len2, result_len3, ))
@@ -43,7 +49,8 @@ def run_simu4d():
     data = f["data3"][:]*1.0 # nqx, nqy, nqz, nomega
     #print "size of data is", data.shape
     #data = np.sum(data[:, :, :, :],axis=2)
-    condition = np.ones(data.shape, dtype=bool)
+    condition = np.ones(data.shape, dtype=np.int32)
+    print condition.dtype
     
     n = np.sum(data)*1.0
     print "n=", n
@@ -53,14 +60,15 @@ def run_simu4d():
     maxywidth = np.min(np.sum(condition, axis=1)) / 9
     maxzwidth = np.min(np.sum(condition, axis=2)) / 9
     maxowidth = np.min(np.sum(condition, axis=3)) / 9
-    maxxwidth = 3
+    maxxwidth = 2
     maxywidth = 2
     maxzwidth = 2
-    maxowidth = 2
+    maxowidth = 1
     
     maxw = np.array([maxxwidth, maxywidth, maxzwidth, maxowidth])
-    A = np.cumsum(np.cumsum(np.cumsum(np.cumsum(data, axis=0), axis=1), axis=2), axis=3)
-    Cn, kaves, deltas = calc_cost4d_f90(A, maxw, data, condition)
+    #!A = np.cumsum(np.cumsum(np.cumsum(np.cumsum(data, axis=0), axis=1), axis=2), axis=3)
+    #Cn, kaves, deltas = calc_cost4d_f90(A, maxw, data, condition)
+    Cn, kaves, deltas = calc_cost4d_f90(maxw, data, condition)
     opt_indx = np.unravel_index(np.argmin(Cn, axis=None), Cn.shape)
     opt_indx = (opt_indx[0] + 1, opt_indx[1] + 1, opt_indx[2] + 1, opt_indx[3] + 1)
     print "opt_indx for Cn", opt_indx
