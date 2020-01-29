9154e755b902f656475f084b0b0e45ff8f6b9384
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2020-01-29 21:23:26 +0900

    gettotalintensity.f90 for checking the total intensity of out_hw_0_1.txt

---
+++ gettotalintensity.f90
@@ -0,0 +1,27 @@
+program gettotalintensity
+  implicit none
+  integer, parameter :: nlines=41352500
+  double precision :: intensity(nlines), qx(nlines), qy(nlines), qz(nlines), e(nlines), sig(nlines)
+  double precision :: xi, xe, dx
+  character(len=80) :: argc
+  character(len=80) :: dummy
+  integer, parameter :: ndum=9
+  integer :: idum, iint, ios
+
+  call getarg(1, argc)
+  print *, trim(argc)
+  open(10, file=trim(argc), status='old')
+  do idum=1, ndum
+     read(10,'(a80)') dummy
+     print *, dummy
+  enddo
+  do iint=1, nlines
+     read(10,*, end=99) qx(iint),qy(iint),qz(iint),e(iint),intensity(iint),sig(iint)
+     !print *, qx(iint),qy(iint),qz(iint),e(iint),intensity(iint),sig(iint)
+  enddo
+  99 print *, "finish reading"
+  print *, "totlal intensity is ",sum(intensity)
+  close(10) 
+
+
+end program gettotalintensity
