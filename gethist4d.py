0381d0c685c092412f26a7d702bffb411d764d0c
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2020-11-05 18:56:35 +0900

    get_ise_auto_fort.py and isefort.py for calculating integrated square errors between pdf and histogram

--- gethist4d.py
+++ gethist4d.py
@@ -110,10 +110,10 @@ def run():
     #ny = 3 # for Ei24
     #nz = 2 # for Ei24
     #ne = 4 # for Ei24
-    nx = 3
+    nx = 4
     ny = 2
-    nz = 2
-    ne = 4
+    nz = 4
+    ne = 3
     head ="./"
     #datafile = head + "out_hw_all.hdf5"
     #datafile = head + "Output4D_00_1560.hdf5"  # for Ei24
