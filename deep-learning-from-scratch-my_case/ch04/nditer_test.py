20cf172d34142000b1c2dc0cdf553b7e281d9fad
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2021-01-20 15:40:25 +0900

    delete deep-learnig dir

--- deep-learning-from-scratch-my_case/ch04/nditer_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import numpy as np
-a = np.arange(10).reshape(2,5)
-#for x in np.nditer(a):
-#    print(x, end=" ")
-
-it = np.nditer(a, flags=['multi_index'])
-for x in it:
-    print(x,"@", it.multi_index)
