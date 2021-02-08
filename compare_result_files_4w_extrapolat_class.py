519384550e024e96c19590206f8ca1c169888a36
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2021-02-08 11:00:42 +0900

    modify compare_result_files_4w_extrapolat_class to plot three datasets in the same figur

--- compare_result_files_4w_extrapolat_class.py
+++ compare_result_files_4w_extrapolat_class.py
@@ -14,6 +14,7 @@ import numpy as np
 class Compare:
 
     def __init__(self, infile1, label1, infile2, label2, dataname, stepsizes,
+                 tcn, cn, ylabel, mnj,
                  dshifti=None, dshiftf=None):
         self.infile1 = infile1
         self.infile2 = infile2
@@ -21,10 +22,14 @@ class Compare:
         self.label2 = label2
         self.dataname = dataname
         self.stepsizes = stepsizes
+        self.tcn = tcn
+        self.cn = cn
+        self.ylabel = ylabel
+        self.mnj = mnj
         self.dshifti = dshifti
         self.dshiftf = dshiftf
-        self.all_data()
-        self.plot_all_data()
+        self.get_all_data()
+        #self.plot_all_data()
 
     def getdata(self, infile):
         nqx = []
@@ -42,36 +47,41 @@ class Compare:
                 nn.append(float(values[4]))
         return np.transpose(np.array([nn, nqx, nqy, nqz, nw]))
 
-    def all_data(self):
+    def get_all_data(self):
         data1 = self.getdata(self.infile1)
         data2 = self.getdata(self.infile2)
         self.all_data = np.zeros((2, data1.shape[0], data1.shape[1]))
         self.all_data[0, :, :] = data1
         self.all_data[1, :, :] = data2
 
-    def plot_all_data(self):
-        fig = plt.figure(figsize=(6, 9))
-        fig.suptitle("extrapolated bin-widths of 4D INS orthotope data "
+    def create_fig(self):
+        self.fig = plt.figure(figsize=(12, 9))
+        self.fig.suptitle("extrapolated bin-widths of 4D INS orthotope data "
                      + self.dataname)
+
+    def plot_all_data(self):
+        #self.fig = plt.figure(figsize=(6, 9))
+        #self.fig.suptitle("extrapolated bin-widths of 4D INS orthotope data "
+        #             + self.dataname)
         #x = np.log10(self.all_data[0, 2:, 0])
         x = self.all_data[0, self.dshifti:self.dshiftf, 0]
         ys = self.all_data[:, self.dshifti:self.dshiftf, 1:]*self.stepsizes
         wlist = ["qx", "qy", "qz", "w"]
 
         for widx, wlabel in enumerate(wlist):
-            ax = fig.add_subplot(4, 1, widx+1)
-            ax.plot(x, ys[1, :, widx],
-                    clip_on=False, linestyle="dotted", lw=1, label=self.label2,
-                    marker=".", ms=4, mec='k', mfc='k', c='k')
+            ax = self.fig.add_subplot(4, self.tcn, self.tcn*widx+self.cn)
             ax.plot(x, ys[0, :, widx],
-                    clip_on=False, linestyle="dotted", lw=1, label=self.label1,
-                    marker="x", ms=4, mec='k', mfc='white', c='k')
+                    clip_on=False, linestyle="dotted", lw=0.8, label=self.label1,
+                    marker="x", ms=5, mec='k', mfc='white', mew=0.8,  c='k')
+            ax.plot(x, ys[1, :, widx],
+                    clip_on=False, linestyle="dotted", lw=0.8, label=self.label2,
+                    marker=".", ms=5, mec='k', mfc='w', mew=0.8, c='k')
             ax.text(np.max(x)*0.5, np.max(ys[:, :, widx])*0.9, wlabel)
             ax.tick_params(labelbottom=False)
-            ax.set_ylim(0, np.max(ys[:, :, widx])*1.06)
+            ax.set_ylim(0, np.max(ys[:, :, widx]) + self.stepsizes[widx])
             mergin = (np.max(np.log10(x))-np.min(np.log10(x)))*0.01
             ax.set_xlim(np.min(x)*10**(-mergin), np.max(x)*10**(mergin))
-            ax.yaxis.set_major_locator(MultipleLocator(self.stepsizes[widx]*4.0))
+            ax.yaxis.set_major_locator(MultipleLocator(self.stepsizes[widx]*self.mnj))
             ax.yaxis.set_minor_locator(MultipleLocator(self.stepsizes[widx]))
             ax.xaxis.set_major_locator(MultipleLocator(1))
             ax.tick_params(top=True, right=True, direction='in', which='both')
@@ -82,13 +92,13 @@ class Compare:
             if widx == 3:
                 ax.tick_params(labelbottom=True)
                 ax.set_xlabel('total count')
-            if widx == 1:
+            if widx == 1 and self.ylabel:
                 ax.set_ylabel('bin width (rlu)')
-            if widx == 3:
+            if widx == 3 and self.ylabel:
                 ax.set_ylabel('bin width (meV)')
-        plt.subplots_adjust(wspace=0.4, hspace=0.0)
+        plt.subplots_adjust(wspace=0.15, hspace=0.0)
         plt.legend(loc="center right")
-        plt.show()
+        #plt.show()
 
 
 #def samplerun():
