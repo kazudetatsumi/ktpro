01b630fbdfdd4eca68386ba0bfbad12e7c43068a
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2021-05-08 14:31:25 +0900

    qens_class energy bug fixed

--- qens_class.py
+++ qens_class.py
@@ -6,8 +6,8 @@
 # This script can apply the adaptive kernel density estimation on the prepared
 # data and show the plots of the results, i.e., the estimated density and the
 # estimated destribution of the kernel widths.
-# This script uses the python methods of the adaptive kenerl denssity estimation
-# distributed at the site linked by Dr. Shimazaki's  HP. 
+# This script uses the python methods of the adaptive kenerl denssity
+# estimation distributed at the site linked by Dr. Shimazaki's  HP.
 # The adaptive kenel density estimation was developped by Dr. Shimazaki.
 # Kazuyoshi TATSUMI 2021/04/03
 import numpy as np
@@ -100,88 +100,79 @@ class qens:
                             (spectra[0, 0, :] <= xlim[1]))[0]
             self.selected_spectra = spectra[0, 1, mask]
             self.selected_energy = spectra[0, 0, mask]
-            self.de = spectra[0, 0, 1] - spectra[0, 0, 0]
-            #plt.plot(spectra[0, 0, mask], spectra[0, 1, mask])
-            #plt.plot(spectra[0, 0, :] - 2.085, spectra[0, 1, :])
-            #plt.yscale('log')
-            #plt.xlim(-0.05025, 0.09975)
-            #plt.show()
 
-        self.xvec = np.array([self.selected_energy[idx] for idx in
+        self.de = spectra[0, 0, 1] - spectra[0, 0, 0]
+
+        # To keep the accuracy, kde is executed on the channel numbers in the
+        # histogram data.
+        # The actual energies were retrieved by "_real" variables.
+        self.xvec = np.array([idx for idx in
                              range(0, self.selected_spectra.shape[0]) for
                              num_repeat in
                              range(0, int(self.selected_spectra[idx]))
                               ], dtype=float)
+        self.xvec_real = np.array([self.selected_energy[idx] for idx in
+                                  range(0, self.selected_spectra.shape[0]) for
+                                  num_repeat in
+                                  range(0, int(self.selected_spectra[idx]))
+                                   ], dtype=float)
 
     def add_shift(self):
-        #print(self.xvec[0:30])
-        self.xvec = self.xvec + np.random.uniform(-0.5, 0.5, size=self.xvec.shape[0])*self.de
+        self.shift = np.random.uniform(-0.5, 0.5, size=self.xvec.shape[0])
+        self.xvec += self.shift
+        self.xvec_real += self.shift*self.de
+
         #print(self.xvec[0:30])
 
     def run_ssvkernel(self):
-        #testx = [4.37, 3.87, 4.00, 4.03, 3.50, 4.08, 2.25, 4.70, 1.73, 4.93, 1.73, 4.62,
-        #     3.43, 4.25, 1.68, 3.92, 3.68, 3.10, 4.03, 1.77, 4.08, 1.75, 3.20, 1.85,
-        #     4.62, 1.97, 4.50, 3.92, 4.35, 2.33, 3.83, 1.88, 4.60, 1.80, 4.73, 1.77,
-        #     4.57, 1.85, 3.52, 4.00, 3.70, 3.72, 4.25, 3.58, 3.80, 3.77, 3.75, 2.50,
-        #     4.50, 4.10, 3.70, 3.80, 3.43, 4.00, 2.27, 4.40, 4.05, 4.25, 3.33, 2.00,
-        #     4.33, 2.93, 4.58, 1.90, 3.58, 3.73, 3.73, 1.82, 4.63, 3.50, 4.00, 3.67,
-        #     1.67, 4.60, 1.67, 4.00, 1.80, 4.42, 1.90, 4.63, 2.93, 3.50, 1.97, 4.28,
-        #     1.83, 4.13, 1.83, 4.65, 4.20, 3.93, 4.33, 1.83, 4.53, 2.03, 4.18, 4.43,
-        #     4.07, 4.13, 3.95, 4.10, 2.72, 4.58, 1.90, 4.50, 1.95, 4.83, 4.12]
-
         #self.y = ssvkernel.ssvkernel(np.array(testx))
         T = (np.max(self.xvec) - np.min(self.xvec))
         dx = np.sort(np.diff(np.sort(self.xvec)))
         dt_samp = dx[np.nonzero(dx)][0]
-        # To relieve the effect of the finite counts near the upper and lower bounds of the measurement range
+        # To relieve the effect of the finite counts near the upper and lower
+        # bounds of the measurement range
         # we extend the range by some fraction of the original range
         #mergin = T*0.2
         #tin = np.linspace(np.min(self.xvec) - mergin, np.max(self.xvec) + mergin,
         #                  int(min(np.ceil(T*1.4 / dt_samp), 1e3)))
+        # tin is the space where densities are inferred.
         tin = np.linspace(np.min(self.xvec), np.max(self.xvec),
-                          int(min(np.ceil(T / dt_samp), 1e3))) 
+                          int(min(np.ceil(T / dt_samp), 1e3)))
+        tin_real = np.linspace(np.min(self.xvec_real), np.max(self.xvec_real),
+                               int(min(np.ceil(T / dt_samp), 1e3)))
 
         self.y = ssvkernel.ssvkernel(self.xvec, tin)
         self.y_ = sskernel.sskernel(self.xvec, tin)
-        #print(self.y[0].shape)
-        #print(self.y[1].shape)
-        #print(self.y[2].shape)
-        #print(self.y_[2])
         norms = self.selected_spectra/np.sum(self.selected_spectra)/self.de
-        # channel number is depricated now!
-        #ch = np.arange(0, norms.shape[0])
         fig = plt.figure(figsize=(12, 12))
         ax = fig.add_subplot(3, 1, 1)
-        #ax.bar(ch, norms, width=1.0, label='expt data')
         ax.bar(self.selected_energy, norms, width=self.de, label='expt data')
-        #ax.plot(self.selected_energy, norms)
-        ax.plot(self.y[1], self.y[0], c='r', label='ssvkernel')
-        ax.plot(self.y_[1], self.y_[0], c='k', label='sskernel')
-        xlim = ax.get_xlim()
-        ax.tick_params(top=True, right=True, direction='in', which='both', labelbottom=False)
+        ax.plot(tin_real, self.y[0]/self.de, c='r', label='ssvkernel')
+        ax.plot(tin_real, self.y_[0]/self.de, c='k', label='sskernel')
+        ax.tick_params(top=True, right=True, direction='in', which='both',
+                       labelbottom=False)
         ax.set_ylabel('density')
         ax.set_xlabel('energy ($\mu eV$)')
         plt.legend()
         ax = fig.add_subplot(3, 1, 2)
         ax.set_ylabel('density')
-        #ax.set_xlabel('# of energy channel')
         ax.bar(self.selected_energy, norms, width=self.de, label='expt data')
-        ax.plot(self.y[1], self.y[0], c='r', label='ssvkernel')
-        ax.plot(self.y_[1], self.y_[0], c='k', label='sskernel')
+        ax.plot(tin_real, self.y[0]/self.de, c='r', label='ssvkernel')
+        ax.plot(tin_real, self.y_[0]/self.de, c='k', label='sskernel')
         ax.set_yscale('log')
-        ax.set_ylim(0.00000001, np.max(self.y_[0]))
-        ax.tick_params(top=True, right=True, direction='in', which='both', labelbottom=False)
-        ax.set_xlim(xlim)
+        ax.set_ylim(0.0001, np.max(self.y_[0])/self.de)
+        ax.tick_params(top=True, right=True, direction='in', which='both',
+                       labelbottom=False)
         plt.legend()
         ax = fig.add_subplot(3, 1, 3)
-        ax.plot(self.y[1], self.y[2], c='r', label='ssvkernel')
-        ax.plot(self.y_[1], np.ones(self.y_[1].shape[0])*self.y_[2], c='k', label='sskernel')
+        ax.plot(tin_real, self.y[2], c='r', label='ssvkernel')
+        ax.plot(tin_real, np.ones(self.y_[1].shape[0])*self.y_[2], c='k',
+                label='sskernel')
         ax.set_ylabel('band-width')
-        #ax.set_xlabel('energy ($\mu eV$)')
-        ax.tick_params(top=True, right=True, direction='in', which='both', labelbottom=True)
-        #ax.set_xlim(xlim)
+        ax.set_xlabel('energy ($\mu eV$)')
+        ax.tick_params(top=True, right=True, direction='in', which='both',
+                       labelbottom=True)
         plt.subplots_adjust(hspace=0.0)
-        ax.set_xlim(xlim)
         plt.legend()
         plt.show()
 
@@ -189,6 +180,8 @@ class qens:
         dataset = {}
         dataset['y_ssvk'] = self.y
         dataset['y_ssk'] = self.y_
+        dataset['xlim'] = np.array([np.min(self.xvec_real),
+                                   np.max(self.xvec_real)])
         with open(output_file, 'wb') as f:
             pickle.dump(dataset, f, -1)
 
