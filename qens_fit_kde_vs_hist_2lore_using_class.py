6bc7aef5c45be0c22f5a82718b96fe964b4cb999
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2021-12-14 17:29:47 +0900

    saving qens_fit_kde_vs_hist_2lore_using_class for different initial parameters

--- qens_fit_kde_vs_hist_2lore_using_class.py
+++ qens_fit_kde_vs_hist_2lore_using_class.py
@@ -4,7 +4,7 @@
 # For histograms, the background is fixed as the bkg_peak_ratio(kde) * peak(histogram)
 # as suggested by Dr. Matsuura.
 # This is for the fitting analysis using 2 Lorentzian components. 
-# example usage qens_fit_kde_vs_hist_2lore_using_class.py 6202 0000025io 0875
+# example usage qens_fit_kde_vs_hist_2lore_using_class.py 6202 6204 0000025io 0875
 # Kazuyoshi TATSUMI 2021/12/05
 import numpy as np
 import sys 
@@ -32,12 +32,17 @@ def sqrun_kde_hist_2lore():
         print("using default runno 6202")
         runno = "6202"
     if len(sys.argv) >= 3:
-        dirname = sys.argv[2]
+        runnod = sys.argv[2]
+    else:
+        print("using default runnod 6204")
+        runnod = "6204"
+    if len(sys.argv) >= 4:
+        dirname = sys.argv[3]
     else:
         print("using default dirname 000025io")
         dirname = "000025io"
-    if len(sys.argv) >= 4:
-        frac = sys.argv[3]
+    if len(sys.argv) >= 5:
+        frac = sys.argv[4]
     else:
         print("using default frac """)
         frac = ""
@@ -45,8 +50,8 @@ def sqrun_kde_hist_2lore():
     #head = "/home/kazu/desktop/210108/Tatsumi/pickles/"+dirname+"/"
     #note that we use the device function collected in the total measument time.
     head = "./"
-    devf = head + "qens_kde_o_divided_by_i_6204.pkl"
-    tf = head + "qens_kde_o_divided_by_i_6202"+frac+".pkl"
+    devf = head + "qens_kde_o_divided_by_i_"+runnod+".pkl"
+    tf = head + "qens_kde_o_divided_by_i_"+runno+".pkl"
     elim = [-0.03, 0.07]
     proj = qfc.qens_fit(devf, tf, elim, showplot=False)
     proj.icorr()
@@ -55,16 +60,16 @@ def sqrun_kde_hist_2lore():
     proj.optimize(variables=[1.67780642e-04, 3.08144561e-02, 1.16049540e-04, 7.85885231e-03, 2.04665269e-01, 4.17453525e+00], figname="qens_kde_fit2.png")
 
     head = "/home/kazu/desktop/210108/Tatsumi/srlz/"+dirname+"/"
-    devf = head + "qens_hist_o_divided_by_i_6204.pkl"
-    tf = head + "qens_hist_o_divided_by_i_6202"+frac+".pkl"
+    devf = head + "qens_hist_o_divided_by_i_"+runno+".pkl"
+    tf = head + "qens_hist_o_divided_by_i_"+runno+frac+".pkl"
     proj.devf = devf
     proj.tf = tf
     proj.icorr()
     proj.preprocessh(doicorr=True)
     #proj.optimize(variables=[1.46103037e-04, 1.23754329e-02, 5.20429443e-01],
     #        figname="qens_hist_fit.png")
-    proj.optimize(variables=[1.67780642e-04, 3.08144561e-02, 1.16049540e-04, 7.85885231e-03, 2.04665269e-01], figname="qens_hist_fit2_basefixed.png")
-    proj.optimize(variables=[1.67780642e-04, 3.08144561e-02, 1.16049540e-04, 7.85885231e-03, 2.04665269e-01, 4.17453525e+00], figname="qens_hist_fit2.png")
+    proj.optimize(variables=[1.32501760e-06, 2.61859941e-02, 2.15895618e-07, 4.50505630e-03, 0.71572410e-03], figname="qens_hist_fit2_basefixed.png")
+    proj.optimize(variables=[1.32501760e-06, 2.61859941e-02, 2.15895618e-07, 4.50505630e-03, 0.71572410e-03, 1.77283308e-04], figname="qens_hist_fit2.png")
 
 
 sqrun_kde_hist_2lore()
