e1762ef9f635d9d036f4790297d22298d9713030
Kazuyoshi TATSUMI <kazu@machp.local>
2022-02-24 23:33:25 +0900

    ssvkernel compiled

--- ssvkernel.f90
+++ ssvkernel.f90
@@ -1,220 +1,248 @@
-!fortran 90 test program for a kernel density estimation with grobally-optimized bandwidth.
-!Kazuyoshi TATSUMI 2021/12/08
+!fortran 90 test program for a kernel density estimation with localy optimized bandwidths.
+!Kazuyoshi TATSUMI 2022/02/24
 
 module ssvkernel
   use ISO_C_BINDING
   implicit none
   include 'fftw3.f'
-  integer :: xsize
-  integer :: tinsize
+  integer :: xsize, tinsize, M
   double precision, parameter :: pi  = 4 * atan (1.0_8)
 contains
 
-  subroutine ssvk(optw, M, winparam, xsize0, tinsize0, xdat, yh) bind(C, name="ssk")
-	real(c_double), intent(out) :: optw
-	integer, intent(in) :: M, winparam, xsize0, tinsize0
-	double precision, intent(in) :: xdat(xsize0)
-	double precision, intent(inout) :: yh(tinsize0)
+  subroutine ssvk(M0, winparam, xsize0, tinsize0, xdat, optw, yopt) bind(C, name="ssk")
+    integer, intent(in) :: M0, winparam, xsize0, tinsize0
+    double precision, intent(in) :: xdat(xsize0)
+    double precision, intent(out) :: optw(tinsize0), yopt(tinsize0)
     double precision :: xdatstd(xsize0), xdatstddiff(xsize0-1), xdatstddiffstd(xsize0-1)
-    double precision :: tin(tinsize0), thist(tinsize0+1), y_hist(tinsize0), y(tinsize0)
-    double precision T, dt_samp, dt, cost, Wins(M), dw, wi, Win
-    double precision, dimension(M, tinsize0) :: cfxw, optws, C_local
+    double precision :: tin(tinsize0), thist(tinsize0+1), y_hist(tinsize0), yh(tinsize0)
+    double precision T, dt_samp, dt, cost, Wins(M0), dw, wi, Win
+    double precision, dimension(M0, tinsize0) :: cfxw, optws, C_local
     integer :: yhist(tinsize0), minkbwidx(tinsize0)
     integer nbin, nsmpl, i, tinsize2
-	integer kbwidx, winidx, xchidx
-	tinsize=tinsize0
-	xsize=xsize0
-	!data xdat/4.37, 3.87, 4.00, 4.03, 3.50, 4.08, 2.25, 4.70, 1.73, 4.93, 1.73, 4.62, 3.43, 4.25, 1.68, 3.92, 3.68, 3.10, 4.03, 1.77,&
-	!4.08, 1.75, 3.20, 1.85, 4.62, 1.97, 4.50, 3.92, 4.35, 2.33, 3.83, 1.88, 4.60, 1.80, 4.73, 1.77, 4.57, 1.85, 3.52, 4.00, 3.70,&
-	!3.72, 4.25, 3.58, 3.80, 3.77, 3.75, 2.50, 4.50, 4.10, 3.70, 3.80, 3.43, 4.00, 2.27, 4.40, 4.05, 4.25, 3.33, 2.00, 4.33, 2.93,&
-	!4.58, 1.90, 3.58, 3.73, 3.73, 1.82, 4.63, 3.50, 4.00, 3.67, 1.67, 4.60, 1.67, 4.00, 1.80, 4.42, 1.90, 4.63, 2.93, 3.50, 1.97,&
-	!4.28, 1.83, 4.13, 1.83, 4.65, 4.20, 3.93, 4.33, 1.83, 4.53, 2.03, 4.18, 4.43, 4.07, 4.13, 3.95, 4.10, 2.72, 4.58, 1.90, 4.50,&
-	!1.95, 4.83, 4.12/
-	T=maxval(xdat)-minval(xdat)
-	xdatstd=xdat
-	call quicksort(xdatstd, 1, xsize)
-	xdatstddiff=xdatstd(2:xsize)-xdatstd(1:xsize-1)
-	xdatstddiffstd=xdatstddiff
-	call quicksort(xdatstddiffstd, 1, size(xdat)-1)
-	dt_samp=minval(pack(xdatstddiffstd, xdatstddiffstd > 0.))
-	if (ceiling(T/dt_samp) > 1e3) then
-		tinsize2 = 1e3
-	else
-		tinsize2 = ceiling(T/dt_samp)
-	endif
-	print *, 'tinsize2=',tinsize2
-	!allocate(tin(tinsize), thist(tinsize+1), y_hist(tinsize), yh(tinsize), yhist(tinsize), y(tinsize))
-	dt=T/(tinsize-1)
-	tin = (/(((xchidx-1)*dt+minval(xdat)), xchidx=1,tinsize)/)
-	thist(1:tinsize)=tin(:)
-	thist(tinsize+1)=tin(tinsize)+dt
-	thist = thist - dt/2
-	yhist=hist(xdat, thist)
-	nsmpl=sum(yhist)
-	y_hist=real(yhist)/real(nsmpl)/dt
+    integer kbwidx, winidx, xchidx
+    tinsize=tinsize0
+    xsize=xsize0
+    M=M0
+    !data xdat/4.37, 3.87, 4.00, 4.03, 3.50, 4.08, 2.25, 4.70, 1.73, 4.93, 1.73, 4.62, 3.43, 4.25, 1.68, 3.92, 3.68, 3.10, 4.03, 1.77,&
+    !4.08, 1.75, 3.20, 1.85, 4.62, 1.97, 4.50, 3.92, 4.35, 2.33, 3.83, 1.88, 4.60, 1.80, 4.73, 1.77, 4.57, 1.85, 3.52, 4.00, 3.70,&
+    !3.72, 4.25, 3.58, 3.80, 3.77, 3.75, 2.50, 4.50, 4.10, 3.70, 3.80, 3.43, 4.00, 2.27, 4.40, 4.05, 4.25, 3.33, 2.00, 4.33, 2.93,&
+    !4.58, 1.90, 3.58, 3.73, 3.73, 1.82, 4.63, 3.50, 4.00, 3.67, 1.67, 4.60, 1.67, 4.00, 1.80, 4.42, 1.90, 4.63, 2.93, 3.50, 1.97,&
+    !4.28, 1.83, 4.13, 1.83, 4.65, 4.20, 3.93, 4.33, 1.83, 4.53, 2.03, 4.18, 4.43, 4.07, 4.13, 3.95, 4.10, 2.72, 4.58, 1.90, 4.50,&
+    !1.95, 4.83, 4.12/
+    T=maxval(xdat)-minval(xdat)
+    xdatstd=xdat
+    call quicksort(xdatstd, 1, xsize)
+    xdatstddiff=xdatstd(2:xsize)-xdatstd(1:xsize-1)
+    xdatstddiffstd=xdatstddiff
+    call quicksort(xdatstddiffstd, 1, size(xdat)-1)
+    dt_samp=minval(pack(xdatstddiffstd, xdatstddiffstd > 0.))
+    if (ceiling(T/dt_samp) > 1e3) then
+        tinsize2 = 1e3
+    else
+        tinsize2 = ceiling(T/dt_samp)
+    endif
+    print *, 'tinsize2=',tinsize2
+    dt=T/(tinsize-1)
+    tin = (/(((xchidx-1)*dt+minval(xdat)), xchidx=1,tinsize)/)
+    thist(1:tinsize)=tin(:)
+    thist(tinsize+1)=tin(tinsize)+dt
+    thist = thist - dt/2
+    yhist=hist(xdat, thist)
+    nsmpl=sum(yhist)
+    y_hist=real(yhist)/dt
     dw=(ilogexp(T)-ilogexp(winparam*dt))/M
-	! Wins contains all widths to be considered for kernels as well as window functions, 
-	! playing a dual role to put kernel band-widths as well as window-widths.
-	Wins=(/((winidx-1)*dw+ilogexp(winparam * dt), winidx=1,M)/)
+    ! Wins contains all widths to be considered for kernels as well as window functions, 
+    ! playing a dual role to put kernel band-widths as well as window-widths.
+    Wins=(/((winidx-1)*dw+ilogexp(winparam * dt), winidx=1,M)/)
     print *, "check ssvkernel param", winparam, M
     print *, "check Win", Wins(1:10)
 
-	!integrand of cost func, for fixed kernel band-widths
+    !integrand of cost func, for fixed kernel band-widths
     cfxw=0.
     do kbwidx=1, M
       wi=Wins(kbwidx)
       yh=fftkernel(y_hist, wi/dt)
       cfxw(kbwidx,:)=yh**2 - 2*yh*y_hist + 2./(2*pi)**0.5/wi*y_hist
     enddo
-	!optw is a conversion maxtrix containing an optimum kernel band width for a pair of a window width and a x channel.
+    !optws is a conversion maxtrix containing an optimum kernel band width for a pair of
+    ! a window width and a x channel.
     optws=0.
-	do winidx=1, M    ! do loop wrt window-widths
+    do winidx=1, M    ! do loop wrt window-widths
       Win=Wins(winidx)
-	  C_local=0.
-	  do kbwidx=1, M   ! do loop wrt kernel band-widths
-	     !C_local(kbwidx, :)=fftkernelWin(c[kbwidx,:], Win/dt, 'Gauss')
-	  enddo
-	  minkbwidx=minloc(C_local, 1)  
-	  do xchidx=1, tinsize ! do loop wrt x channels
-	     optws(winidx, xchidx) = Wins(minkbwidx(xchidx))
-	  enddo
+      C_local=0.
+      do kbwidx=1, M   ! do loop wrt kernel band-widths
+         C_local(kbwidx, :)=fftkernel(cfxw(kbwidx,:), Win/dt)
+      enddo
+      minkbwidx=minloc(C_local, 1)  
+      do xchidx=1, tinsize ! do loop wrt x channels
+         optws(winidx, xchidx) = Wins(minkbwidx(xchidx))
+      enddo
     enddo
-
-
-	call opt(optw, yh, y_hist, xdat, nsmpl, dt)
-	write(*, '(f12.5)') optw
+    call opt(optw, yopt, y_hist, xdat, nsmpl, tin, dt, Wins, optws)
   end subroutine ssvk
 
   function hist(x, th)
-	double precision, intent(in) :: x(xsize), th(tinsize+1)
-	integer :: hist(tinsize)
-	integer ix, it
-	hist(:) = 0
-	do ix = 1, xsize
-	   do it = 1, tinsize
-	      if ( x(ix) >= th(it) .and. x(ix) < th(it+1) ) then
-			   hist(it) = hist(it) + 1
-		  end if
-	   end do
-	end do
+    double precision, intent(in) :: x(xsize), th(tinsize+1)
+    integer :: hist(tinsize)
+    integer ix, it
+    hist(:) = 0
+    do ix = 1, xsize
+       do it = 1, tinsize
+          if ( x(ix) >= th(it) .and. x(ix) < th(it+1) ) then
+               hist(it) = hist(it) + 1
+          end if
+       end do
+    end do
   end function hist
-	
+    
   subroutine plothist(yhist)
-	integer, intent(in) ::  yhist(tinsize)
-	integer ix, j
-	do ix = 1, tinsize
+    integer, intent(in) ::  yhist(tinsize)
+    integer ix, j
+    do ix = 1, tinsize
       print *, ("*", j=1,yhist(ix))
-	end do
+    end do
   end subroutine plothist
 
-  subroutine opt(optw, y, y_hist, xdat, nsmpl, dt)
-	double precision, intent(in) :: y_hist(tinsize), dt, xdat(xsize)
-	integer, intent(in) :: nsmpl
-	integer, parameter :: maxiter = 20
-	double precision, parameter :: tol = 10e-5
-	double precision, parameter :: phi = (5**0.5 + 1) / 2
-	double precision :: cost(maxiter), Win(maxiter), dummy(tinsize), yh1(tinsize), yh2(tinsize)
-	double precision, intent(out) :: y(tinsize), optw
-	double precision :: Winmin, Winmax, a, b, c1, c2, f1, f2
-	integer :: kiter
-	kiter=1
-	cost=0.
-	Win=0.
-	Winmin = 2*dt
-	Winmax = maxval(xdat) - minval(xdat)
-	a=ilogexp(Winmin)
-	b=ilogexp(Winmax)
-	c1=(phi - 1)*a + (2 - phi)*b
-	c2=(2 - phi)*a + (phi - 1)*b
-	call costfunction(dummy, f1, y_hist, nsmpl, logexp(c1), dt)
-	call costfunction(dummy, f2, y_hist, nsmpl, logexp(c2), dt)
-	do while ( (abs(a-b) > tol*(abs(c1)+abs(c2))) .and. (kiter <= maxiter) )
-	  if (f1 < f2) then
-	     b=c2
-		 c2=c1
-		 c1=(phi-1)*a + (2-phi)*b
-		 f2=f1
-		 call costfunction(yh1, f1, y_hist, nsmpl, logexp(c1), dt)
-		 Win(kiter)=logexp(c1)
-		 cost(kiter)=f1
-		 optw=logexp(c1)
-		 y=yh1/sum(yh1*dt)
-	  else
-		 a=c1
-		 c1=c2
-		 c2=(2-phi)*a + (phi-1)*b
-		 f1=f2
-		 call costfunction(yh2, f2, y_hist, nsmpl, logexp(c2), dt)
-		 Win(kiter)=logexp(c2)
-		 cost(kiter)=f2
-		 optw=logexp(c2)
-		 y=yh2/sum(yh2*dt)
-	  endif
-	  print *, kiter, cost(kiter), optw
-	  kiter=kiter+1
-	enddo
+  subroutine opt(optw, yopt, y_hist, xdat, nsmpl, tin, dt, Wins, optws)
+    double precision, intent(in) :: y_hist(tinsize), dt, xdat(xsize), tin(tinsize)
+    double precision, intent(in) :: Wins(M), optws(M, tinsize)
+    integer, intent(in) :: nsmpl
+    integer, parameter :: maxiter = 30
+    double precision, parameter :: tol = 10e-5
+    double precision, parameter :: phi = (5**0.5 + 1) / 2
+    double precision :: cost(maxiter), gs(maxiter)
+    double precision, dimension(tinsize) :: dummy, dummy2, yh1, yh2, optwp1, optwp2, yv1, yv2
+    double precision, intent(out) :: yopt(tinsize), optw(tinsize)
+    double precision :: a, b, c1, c2, f1, f2
+    integer :: kiter
+    kiter=1
+    gs=0.
+    cost=0.
+    a=1e-12
+    b=1
+    c1=(phi - 1)*a + (2 - phi)*b
+    c2=(2 - phi)*a + (phi - 1)*b
+    call costfunction(f1, dummy, dummy2, y_hist, nsmpl, tin, dt, optws, Wins, c1)
+    call costfunction(f2, dummy, dummy2, y_hist, nsmpl, tin, dt, optws, Wins, c2)
+    do while ( (abs(a-b) > tol*(abs(c1)+abs(c2))) .and. (kiter <= maxiter) )
+      if (f1 < f2) then
+         b=c2
+         c2=c1
+         c1=(phi-1)*a + (2-phi)*b
+         f2=f1
+         call costfunction(f1, yv1, optwp1, y_hist, nsmpl, tin, dt, optws, Wins, c1)
+         yopt=yv1/sum(yv1*dt)
+         optw=optwp1
+      else
+         a=c1
+         c1=c2
+         c2=(2-phi)*a + (phi-1)*b
+         f1=f2
+         call costfunction(f2, yv2, optwp2, y_hist, nsmpl, tin, dt, optws, Wins, c2)
+         yopt=yv2/sum(yv2*dt)
+         optw=optwp1
+      endif
+      !print *, kiter, cost(kiter), optw
+      gs(kiter)=c1
+      cost(kiter)=f1
+      kiter=kiter+1
+    enddo
   end subroutine opt
 
-  subroutine costfunction(yh, cost, y_hist, nsmpl, w, dt)
-	integer, intent(in) :: nsmpl
-	double precision, intent(in) ::  y_hist(tinsize), w, dt
-	double precision, intent(out) :: yh(tinsize), cost
-	yh=0.
-	yh=fftkernel(y_hist, w/dt)
-	cost=sum(yh**2)*dt - 2*sum(yh*y_hist)*dt + 2/(2*pi)**0.5/w/nsmpl
-	cost=cost*nsmpl**2
+  subroutine costfunction(Cg, yv, optwp, y_hist, nsmpl, tin, dt, optws, Wins, g)
+    integer, intent(in) :: nsmpl
+    double precision, dimension(tinsize), intent(in) ::  y_hist, tin
+    double precision, intent(in) :: optws(M, tinsize), Wins(M), dt, g
+    double precision, intent(out) :: Cg, yv(tinsize), optwp(tinsize)
+    double precision, dimension(tinsize) :: optwv, gammas, Z
+    double precision, allocatable :: y_hist_nz(:), tin_nz(:)
+    double precision :: cintegrand(tinsize)
+    integer :: xchidx, maxidx, wchidx
+    optwv=0.
+    do xchidx=1, tinsize
+      gammas = optws(:, xchidx)/Wins
+      if (g > maxval(gammas)) then
+        optwv(xchidx)=minval(Wins)
+      else
+        if (g < minval(gammas)) then
+          optwv(xchidx)=maxval(Wins)
+        else
+          maxidx=maxval(pack([(wchidx, wchidx=1, M)], gammas >= g))
+          optwv(xchidx)=g*Wins(maxidx)
+        endif
+      endif
+    enddo
+    optwp=0.
+    do xchidx=1, tinsize
+      Z=Gauss(tin(xchidx)-tin, optwv/g)
+      optwp(xchidx)=sum(optwv*Z)/sum(Z)
+    enddo
+    y_hist_nz=pack(y_hist, y_hist > 0.) 
+    tin_nz=pack(tin, y_hist>0)
+    yv = 0.
+    do xchidx=1, tinsize
+      yv(xchidx)=sum(y_hist_nz*dt*Gauss(tin(xchidx)-tin_nz, optwp(xchidx)))
+    enddo
+    yv=yv*nsmpl/sum(yv*dt)
+    cintegrand = yv**2 - 2.*yv*y_hist + 2./(2.*pi)**0.5/optwp*y_hist
+    Cg=sum(cintegrand*dt)
   end subroutine costfunction
 
   function fftkernel(x, w)
-	!include 'fftw3.f'
-	double precision, intent(in) :: x(tinsize), w
-	double precision :: fftkernel(tinsize), Lmax
-	integer :: L, N, i
-	integer(8) :: plan
-	double complex, allocatable :: input(:), output(:), xinput(:), Xoutput(:), Youtput(:), K(:)
-	double precision, allocatable :: f(:), expa(:)
-	L=tinsize
-	Lmax=L+3*w
-	N = 2**ceiling(log(Lmax)/log(2.))
-	allocate(input(N), output(N), xinput(N), Xoutput(N), f(N), expa(N), K(N), Youtput(N))
-	call dfftw_plan_dft_1d(plan, N, input, output, FFTW_FORWARD, FFTW_ESTIMATE)
-	xinput(1:tinsize)=x
-	xinput(tinsize+1:N)=0.0
-	call dfftw_execute_dft(plan, xinput, Xoutput)
-	f(1:N/2+1) = -1.0*real((/(i, i=0, N/2, 1)/))/real(N)
-	f(N/2+2:N) = 0.5 - real((/(i, i=1, N/2 - 1, 1)/))/real(N)
-	! for preventing the procedure from underflow exception.
-	expa=-0.5*(w*2*pi*f)**2
-	K = 0.
-	where (expa > -708) K=exp(expa)
-	call dfftw_destroy_plan(plan)
-	call dfftw_plan_dft_1d(plan, N, input, output, FFTW_BACKWARD, FFTW_ESTIMATE)
-	call dfftw_execute_dft(plan, Xoutput*K, Youtput)
-	call dfftw_destroy_plan(plan)
-	! Computing a fftw forward followed by a fftw backward results in the original
+    double precision, intent(in) :: x(tinsize), w
+    double precision :: fftkernel(tinsize), Lmax
+    integer :: L, N, i
+    integer(8) :: plan
+    double complex, allocatable :: input(:), output(:), xinput(:), Xoutput(:), Youtput(:), K(:)
+    double precision, allocatable :: f(:), expa(:)
+    L=tinsize
+    Lmax=L+3*w
+    N = 2**ceiling(log(Lmax)/log(2.))
+    allocate(input(N), output(N), xinput(N), Xoutput(N), f(N), expa(N), K(N), Youtput(N))
+    call dfftw_plan_dft_1d(plan, N, input, output, FFTW_FORWARD, FFTW_ESTIMATE)
+    xinput(1:tinsize)=x
+    xinput(tinsize+1:N)=0.0
+    call dfftw_execute_dft(plan, xinput, Xoutput)
+    f(1:N/2+1) = -1.0*real((/(i, i=0, N/2, 1)/))/real(N)
+    f(N/2+2:N) = 0.5 - real((/(i, i=1, N/2 - 1, 1)/))/real(N)
+    ! for preventing the procedure from underflow exception.
+    expa=-0.5*(w*2*pi*f)**2
+    K = 0.
+    where (expa > -708) K=exp(expa)
+    call dfftw_destroy_plan(plan)
+    call dfftw_plan_dft_1d(plan, N, input, output, FFTW_BACKWARD, FFTW_ESTIMATE)
+    call dfftw_execute_dft(plan, Xoutput*K, Youtput)
+    call dfftw_destroy_plan(plan)
+    ! Computing a fftw forward followed by a fftw backward results in the original
     ! array scaled by N. So, we should divid the result by N so as to obtain the
-	! count dependent cost fucntion.
-	fftkernel=real(Youtput(1:L)/N)
+    ! count dependent cost fucntion.
+    fftkernel=real(Youtput(1:L)/N)
   end function fftkernel
 
+  function Gauss(x, w)
+    double precision :: Gauss(tinsize)
+    double precision, intent(in) :: x(tinsize), w(tinsize)
+    Gauss =  1. / (2 * pi)**2 / w * exp(-x**2 / 2. / w**2)
+  end function Gauss
+
   function ilogexp(x)
-	double precision, intent(in) :: x
-	double precision :: ilogexp
-	if ( x < 1e2 ) then
+    double precision, intent(in) :: x
+    double precision :: ilogexp
+    if ( x < 1e2 ) then
        ilogexp = log(exp(x) - 1)
-	else
-	   ilogexp = x
-	endif 
+    else
+       ilogexp = x
+    endif 
   end function ilogexp
 
   function logexp(x)
-	double precision, intent(in) :: x
-	double precision :: logexp
-	if ( x < 1e2 ) then
+    double precision, intent(in) :: x
+    double precision :: logexp
+    if ( x < 1e2 ) then
        logexp = log(exp(x) + 1)
-	else
-	   logexp = x
-	endif 
+    else
+       logexp = x
+    endif 
   end function logexp
 
 ! quicksort.f -*-f90-*-
