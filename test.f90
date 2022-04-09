61cb9743e4f614d7e08f8648012b16e0f5a51c92
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2022-04-09 12:51:28 +0900

    test

--- test.f90
+++ test.f90
@@ -3,14 +3,21 @@
 !Kazuyoshi TATSUMI 2022/04/08
 
 program testmpi
-  !include 'mpif.h'
-  !double precision, dimension(16, 4) :: A
-  !double precision, allocatable :: eacha
+  include 'mpif.h'
+  double precision, dimension(16) :: A
+  double precision :: b(8)
   integer :: rank, psize, ierr
-  !call MPI_Init(ierr)
-  !call MPI_Comm_size(MPI_COMM_WORLD, psize, ierr)
-  !call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
-  !print *, rank
+  call MPI_Init(ierr)
+  call MPI_Comm_size(MPI_COMM_WORLD, psize, ierr)
+  call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+  A = 0.
+  do i = 1, 8
+    b(i) = i + rank*32  
+  enddo
+  call mpi_allgather(b, 8, mpi_double_precision, A, 8, mpi_double_precision, mpi_comm_world, ierr)
+  if (rank == 0) then
+      print *, A
+  endif
   stop
   end
 
