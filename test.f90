616d8d80da0b998ec7a3c0b27881bf97361aa1fe
Kazu Tatsumi <k-tatsumi@imass.nagoya-u.ac.jp>
2022-04-09 18:56:41 +0900

    test.f90 for mpi test

--- test.f90
+++ test.f90
@@ -4,7 +4,7 @@
 
 program testmpi
   include 'mpif.h'
-  double precision, dimension(16) :: A
+  double precision, dimension(4, 4) :: A
   double precision :: b(8)
   integer :: rank, psize, ierr
   call MPI_Init(ierr)
@@ -16,8 +16,12 @@ program testmpi
   enddo
   call mpi_allgather(b, 8, mpi_double_precision, A, 8, mpi_double_precision, mpi_comm_world, ierr)
   if (rank == 0) then
-      print *, A
+      print *, A(:,1)
+      print *, A(:,2)
+      print *, A(:,3)
+      print *, A(:,4)
   endif
+  call MPI_Finalize(ierr)
   stop
   end
 
