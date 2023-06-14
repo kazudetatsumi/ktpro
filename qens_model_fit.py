2a6b66ed462c186a187fa4b37213a39de97cf016
kazu <kazu@machp.local>
2023-06-15 02:12:54 +0900

    qens_model_fit stdhess is obtained

--- qens_model_fit.py
+++ qens_model_fit.py
@@ -14,7 +14,8 @@ class qens_model_fit(qbr):
         self.q2 = (np.arange(self.qsize)*0.1 + 0.2 + 0.05)**2.
 
     def getdata(self, runNo):
-        preprefix = "/home/kazu/desktop/210108/Tatsumi/"
+        # preprefix = "/home/kazu/desktop/210108/Tatsumi/"
+        preprefix = "/Users/kazu/Desktop/210108/Tatsumi/"
         self.orgprefix = preprefix + "from_pca03/wcorr/test/100/orgs/"
         self.stdprefix = preprefix + "from_pca03/wcorr/test/100/qs/"
         self.kdeprefix = preprefix + "winparam_exam_" + str(runNo) + \
@@ -31,7 +32,9 @@ class qens_model_fit(qbr):
         errork = self.readerror('kde', runNo)
         self.kdefile = self.kdeprefix + "kde2.log"
         maskk, gammak = self.readlog()
-        return maskh, gammah, maskkb, gammakb, maskk, gammak, errorh, errork
+        stdhes = self.readstdhessfromlog(runNo)
+        return maskh, gammah, maskkb, gammakb, maskk, gammak, errorh, errork,\
+            stdhes
 
     def stats(self):
         orderidx1 = np.argsort(self.outall[:, 1])
@@ -59,73 +62,102 @@ class qens_model_fit(qbr):
             gamma.append(float(line[:-1].split('[')[2].split()[1]))
         return np.array(mask), np.array(gamma)
 
+    def readstdhessfromlog(self, runNo):
+        std = []
+        linecount = 999999
+        for line in open(self.orgprefix + "std-hist-" + str(runNo)):
+            if "cov**0.5" in line:
+                linecount = 1
+            elif linecount > 0:
+                linecount -= 1
+            elif linecount == 0:
+                std.append(float(line[:-1].split()[1]))
+                linecount = 999999
+        return np.array(std)
+
     def res(self, coeffs, x, t, error, mask):
         [D, tau] = coeffs
         y = D*x[mask]/(1. + D*tau*x[mask])
         return ((t[mask] - y)/error[mask])
-        #return t[mask] - y
 
     def optimize(self, variables, gamma, error, mask):
-        return so.least_squares(self.res, variables, bounds=(0, np.inf),  args=(self.q2, gamma, error, mask))
+        return so.least_squares(self.res, variables, bounds=(0, np.inf),
+                                args=(self.q2, gamma, error, mask))
 
     def plotter(self, fig, nr, pnr,  x, y, t, e, mask, title, runNo):
         heavymask = ~mask
+        #xerr = 2.*(np.arange(self.qsize)*0.1 + 0.05)*0.05
         ax = fig.add_subplot(nr, len(self.runNos), pnr)
-        ax.errorbar(x[mask], t[mask], yerr=e[mask], marker="x", ms=2, elinewidth=1, lw=0, capsize=3)
-        ax.errorbar(x[heavymask], t[heavymask], yerr=e[heavymask], marker="x", ms=2, elinewidth=1, lw=0, capsize=3, c='gray')
+        ax.errorbar(x[mask], t[mask], yerr=e[mask], marker="x", ms=2,
+                    elinewidth=1, lw=0, capsize=3)
+        #ax.errorbar(x[heavymask], t[heavymask], yerr=e[heavymask], marker="x",
+        #            ms=2, elinewidth=1, lw=0, capsize=3, c='gray')
         ax.text(0.1, 0.017, title+"_"+str(runNo))
         ax.set_ylim(0., 0.022)
         ax.set_xlim(0., 1.6)
         ax.set_yticks([0.000, 0.005, 0.010, 0.015, 0.020])
         ax.plot(x, y)
         if pnr >= (nr-1)*len(self.runNos)+1:
-            ax.tick_params(direction='in', top=True, right=True, labelbottom=True)
+            ax.tick_params(direction='in', top=True, right=True,
+                           labelbottom=True)
         else:
-            ax.tick_params(direction='in', top=True, right=True, labelbottom=False)
+            ax.tick_params(direction='in', top=True, right=True,
+                           labelbottom=False)
 
     def run(self):
         fig = plt.figure(figsize=(10, 10))
         for cidx, runNo in enumerate(self.runNos):
             if runNo == 6202:
-                mask = np.array([False, True, True, True, True, True, True, True, False, False, False])
+                mask = np.array([False, True, True, True, True, True, True,
+                                 True, False, False, False])
             else:
-                mask = np.array([False, True, True, True, True, True, True, True, True, True, True])
+                mask = np.array([False, True, True, True, True, True, True,
+                                 True, True, True, True])
             self.eachrun(cidx, runNo, mask, fig)
 
         plt.subplots_adjust(wspace=0.2, hspace=0.0)
         plt.show()
 
     def eachrun(self, cidx, runNo, mask, fig):
-        maskh, gammah, maskkb, gammakb, maskk, gammak, errorh, errork =\
-            self.getdata(runNo)
-        print(runNo, errorh[0])
+        maskh, gammah, maskkb, gammakb, maskk, gammak, errorh, errork, stdhes\
+            = self.getdata(runNo)
 
-        out = self.optimize([0.05, 52], gammah, errorh[0], mask*~maskh)
+        if runNo < 6206:
+            maskhh = np.array([False, True, True, True, True, True, True,
+                               True, True, True, True])
+        else:
+            maskhh = np.array([False, False, True, True, True, True, True,
+                               True, True, True, True])
+
+        out = self.optimize([0.05, 52], gammah, errorh[0], mask*~maskh*maskhh)
+        #out = self.optimize([0.05, 52], gammah, stdhes, mask*~maskh)
         D = out.x[0]
         tau = out.x[1]
         y = D*self.q2/(1. + D*tau*self.q2)
-        self.plotter(fig, 3, 1+cidx, self.q2, y, gammah, errorh[0], mask*~maskh, 'hist', runNo)
+        self.plotter(fig, 3, 1+cidx, self.q2, y, gammah, errorh[0],
+        #self.plotter(fig, 3, 1+cidx, self.q2, y, gammah, stdhes,
+                     mask*~maskh*maskhh, 'hist', runNo)
 
         out = self.optimize([0.05, 40], gammakb, errork[0], mask*~maskkb)
         D = out.x[0]
         tau = out.x[1]
         y = D*self.q2/(1. + D*tau*self.q2)
-        self.plotter(fig, 3, 1+len(self.runNos)+cidx, self.q2, y, gammakb, errork[0], mask*~maskkb, 'kde_balloon', runNo)
+        self.plotter(fig, 3, 1+len(self.runNos)+cidx, self.q2, y, gammakb,
+                     errork[0], mask*~maskkb, 'kde_balloon', runNo)
 
         out = self.optimize([0.05, 40], gammak, errork[0], mask*~maskk)
         D = out.x[0]
         tau = out.x[1]
         y = D*self.q2/(1. + D*tau*self.q2)
-        self.plotter(fig, 3, 1+len(self.runNos)*2+cidx, self.q2, y, gammak, errork[0], mask*~maskk, 'kde', runNo)
+        self.plotter(fig, 3, 1+len(self.runNos)*2+cidx, self.q2, y, gammak,
+                     errork[0], mask*~maskk, 'kde', runNo)
 
 
 def testrun():
-    #runNos = [6202, 6205, 6203, 6206, 6207]
-    runNos = [6202, 6205]
+    runNos = [6202, 6205, 6203, 6206, 6207]
     qsize = 11
     prj = qens_model_fit(runNos, qsize)
     prj.run()
-
 
 
 testrun()
