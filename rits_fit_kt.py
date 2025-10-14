a773af905fc84dcdd2979e4e3640217c16287338
kazudetatsumi <kazu_tatumi@yahoo.co.jp>
2025-10-14 18:41:30 +0900

    still strugling in rits_fit_kt

--- rits_fit_kt.py
+++ rits_fit_kt.py
@@ -85,29 +85,70 @@ def get_sim_edgespectrum_local2(para):
     return tofIn, yCalc
 
 
-#def test_fit(inpfile, y):
-#    para = get_para(inpfile)
-#    variables = para[4:6]
-#    results = so.least_squares(res, variables, para, y)
-#    print(results)
+def err_transmission(sample, openbeam, fac):
+    err_sample = sample**0.5
+    err_openbeam = openbeam**0.5
+    return ((1./openbeam*err_sample)**2 +
+            (sample/openbeam**2*err_openbeam)**2)**0.5*fac
 
 
-#def res(variables, para, y):
-#    para[4] = variables[0]
-#    para[5] = variables[1]
-#    flight = para[15]
-#    thkl = 2.*para[4]*flight*1.0E6/3956.
-#    sigm, alph, beta = setFacilityParameter(para)
-#    tofIn = np.arange(152)*20+23000
-#    delt = tofIn - thkl
-#    Pu = 0.5 * alph * (alph*sigm*sigm + 2.*delt)
-#    Pv = 0.5 * beta * (beta*sigm*sigm - 2.*delt)
-#    Py = (alph*sigm*sigm + delt) / (np.sqrt(2.) * sigm)
-#    Pz = (beta*sigm*sigm - delt) / (np.sqrt(2.) * sigm)
-#    Pw = delt / (np.sqrt(2.) * sigm)
-#    step = 0.5*sp.erfc(Pw) - 0.5 * (beta*np.exp(Pu)*sp.erfc(Py) - alph * np.exp(Pv)*sp.erfc(Pz)) / (alph + beta)
-#    yCalc = np.exp((-1.) * ((step*(para[2] + para[3] * (1.0E-4)*tofIn)) + (para[0] + para[1] * (1.0E-4)*tofIn)))
-#    return np.sum(((yCalc - y[0])/y[1])**2)
+def test_fit(inpfile):
+    import pickle
+    with open('bi3d_scratch_255_partial_phantom_local_mean.pkl', 'rb') as f:
+        sample = pickle.load(f)
+        op = pickle.load(f)
+    with open('../params_scratch.pkl', 'rb') as f:
+        params = pickle.load(f)
+    #print(sample.shape)
+    #print(params[4:6, 30, 100])
+    y = ((sample/op)[:, 100, 30], err_transmission(sample[:, 100, 30], op[:, 100, 30], 1.))
+    para = get_para(inpfile)
+    variables = para[4:6]
+    checkres(variables, para, y)
+    #print(variables)
+    #results = so.least_squares(res, variables, loss='linear', f_scale=0.1, args=(para, y))
+    #print(results)
+
+
+def checkres(variables, para, y):
+    import matplotlib.pyplot as plt
+    para[4] = variables[0]
+    para[5] = variables[1]
+    flight = para[15]
+    thkl = 2.*para[4]*flight*1.0E6/3956.
+    sigm, alph, beta = setFacilityParameter(para)
+    tofIn = np.arange(152)*20+23000
+    delt = tofIn - thkl
+    Pu = 0.5 * alph * (alph*sigm*sigm + 2.*delt)
+    Pv = 0.5 * beta * (beta*sigm*sigm - 2.*delt)
+    Py = (alph*sigm*sigm + delt) / (np.sqrt(2.) * sigm)
+    Pz = (beta*sigm*sigm - delt) / (np.sqrt(2.) * sigm)
+    Pw = delt / (np.sqrt(2.) * sigm)
+    step = 0.5*sp.erfc(Pw) - 0.5 * (beta*np.exp(Pu)*sp.erfc(Py) - alph * np.exp(Pv)*sp.erfc(Pz)) / (alph + beta)
+    yCalc = np.exp((-1.) * ((step*(para[2] + para[3] * (1.0E-4)*tofIn)) + (para[0] + para[1] * (1.0E-4)*tofIn)))
+    plt.plot(yCalc)
+    plt.plot(y[0])
+    plt.ylim([0, 1])
+    plt.show()
+    #return (yCalc - y[0])/y[1]
+
+
+def res(variables, para, y):
+    para[4] = variables[0]
+    para[5] = variables[1]
+    flight = para[15]
+    thkl = 2.*para[4]*flight*1.0E6/3956.
+    sigm, alph, beta = setFacilityParameter(para)
+    tofIn = np.arange(152)*20+23000
+    delt = tofIn - thkl
+    Pu = 0.5 * alph * (alph*sigm*sigm + 2.*delt)
+    Pv = 0.5 * beta * (beta*sigm*sigm - 2.*delt)
+    Py = (alph*sigm*sigm + delt) / (np.sqrt(2.) * sigm)
+    Pz = (beta*sigm*sigm - delt) / (np.sqrt(2.) * sigm)
+    Pw = delt / (np.sqrt(2.) * sigm)
+    step = 0.5*sp.erfc(Pw) - 0.5 * (beta*np.exp(Pu)*sp.erfc(Py) - alph * np.exp(Pv)*sp.erfc(Pz)) / (alph + beta)
+    yCalc = np.exp((-1.) * ((step*(para[2] + para[3] * (1.0E-4)*tofIn)) + (para[0] + para[1] * (1.0E-4)*tofIn)))
+    return (yCalc - y[0])/y[1]
 
 
 def get_para(inpfile):
@@ -199,4 +240,4 @@ def setFacilityParameter(para):
 #plt.xlim([1, 5.1])
 #plt.show()
 
-
+test_fit('edge_3.inp.phantom')
